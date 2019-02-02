a98d034c71460a0c734244a23be25a9177e48ecf
Matti Airas <mairas@iki.fi>
2019-02-02 13:05:04 +0200

    formatting changes

--- src/devices/system_info.h
+++ src/devices/system_info.h
@@ -3,35 +3,34 @@
 
 #include "device.h"
 
-
 class SystemHz : public Device {
-    uint32_t tick_count = 0;
-    uint32_t prev_millis;
-    float system_hz;
-    void tick();
-    void update();
-  public:
-    void enable() override final;
-    float get();
+ public:
+  void enable() override final;
+  float get();
+ private:
+  uint32_t tick_count = 0;
+  uint32_t prev_millis;
+  float system_hz;
+  void tick();
+  void update();
 };
-
 
 class FreeMem : public Device {
-    uint32_t free_mem;
-    void update();
-  public:
-    void enable() override final;
-    uint32_t get();
+ public:
+  void enable() override final;
+  uint32_t get();
+ private:
+  uint32_t free_mem;
+  void update();
 };
-
 
 class Uptime : public Device {
-    float uptime;
-    void update();
-  public:
-    void enable() override final;
-    float get();
+ public:
+  void enable() override final;
+  float get();
+ private:
+  float uptime;
+  void update();
 };
-
 
 #endif
