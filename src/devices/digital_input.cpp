88e02faf19f99be01b9d5d4b458c22d456981610
Matti Airas <mairas@iki.fi>
2019-02-25 21:10:28 +0200

    DigitalInput is split into DigitalInputCounter and DigitalInputValue

--- src/devices/digital_input.cpp
+++ src/devices/digital_input.cpp
@@ -5,25 +5,51 @@
 #include "sensesp.h"
 
 DigitalInput::DigitalInput(
-      uint8_t pin, int pin_mode, int interrupt_type,
-      String id, String schema)
-      : Device{id, schema}, pin{pin}, interrupt_type{interrupt_type} {
+    uint8_t pin, int pin_mode, int interrupt_type,
+    String id, String schema)
+    : Device{id, schema}, pin{pin}, interrupt_type{interrupt_type} {
   pinMode(pin, pin_mode);
   Serial.println(digitalRead(pin));
 }
 
-void DigitalInput::enable() {
+DigitalInputValue::DigitalInputValue(
+    uint8_t pin, int pin_mode, int interrupt_type,
+    String id, String schema)
+    : DigitalInput{pin, pin_mode, interrupt_type, id, schema} {}
+
+void DigitalInputValue::enable() {
   app.onInterrupt(pin, interrupt_type,
-                  std::bind(&DigitalInput::interrupt_handler, this));
+                  [this](){ this->interrupt_handler(); });
 }
 
-void ICACHE_RAM_ATTR DigitalInput::interrupt_handler() {
+void ICACHE_RAM_ATTR DigitalInputValue::interrupt_handler() {
   app.onDelay(0, [this](){
     this->value = digitalRead(this->pin);
     this->notify();
    });
 }
 
-bool DigitalInput::get() {
-  return this->value;
+bool DigitalInputValue::get() {
+  return value;
+}
+
+DigitalInputCounter::DigitalInputCounter(
+    uint8_t pin, int pin_mode, int interrupt_type,
+    uint read_delay,
+    String id, String schema)
+    : DigitalInput{pin, pin_mode, interrupt_type, id, schema},
+      read_delay{read_delay} {}
+
+void DigitalInputCounter::enable() {
+  app.onInterrupt(pin, interrupt_type,
+                  [this](){ this->counter++; });
+  app.onRepeat(read_delay, [this](){
+    value = counter;
+    counter = 0;
+    notify();
+  });
+}
+
+uint DigitalInputCounter::get() {
+  return value;
 }
--- src/devices/digital_input.h
+++ src/devices/digital_input.h
@@ -7,13 +7,36 @@ class DigitalInput : public Device {
  public:
   DigitalInput(uint8_t pin, int pin_mode, int interrupt_type,
                String id="", String schema="");
+ protected:
+  uint8_t pin;
+  int interrupt_type;
+};
+
+// DigitalInputValue is meant to report directly the state of
+// a slowly changing signal
+class DigitalInputValue : public DigitalInput {
+ public:
+  DigitalInputValue(uint8_t pin, int pin_mode, int interrupt_type,
+                    String id="", String schema="");
   void enable() override final;
   bool get();
  private:
   void ICACHE_RAM_ATTR interrupt_handler();
-  uint8_t pin;
-  int interrupt_type;
   bool value = 0;
+};
+
+// DigitalInputCounter tracks rapidly changing digital inputs
+class DigitalInputCounter : public DigitalInput {
+ public:
+  DigitalInputCounter(uint8_t pin, int pin_mode, int interrupt_type,
+                      uint read_delay,
+                      String id="", String schema="");
+  void enable() override final;
+  uint get();
+ private:
+  uint read_delay;
+  volatile uint counter = 0;
+  uint value;
 };
 
 #endif
