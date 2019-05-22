d88c26dbe6f678fc9e486041ecde75381d2b577f
joelkoz <joel.kozikowski@gmail.com>
2019-05-21 21:18:08 -0400

    Removed "final" from Passthrough methods
    Allow for FilteredPassthrough to be a descendant

--- src/transforms/passthrough.h
+++ src/transforms/passthrough.h
@@ -16,7 +16,7 @@ class Passthrough : public OneToOneTransform<T> {
   }
 
 
-  virtual void set_input(T newValue, uint8_t idx = 0) override final {
+  virtual void set_input(T newValue, uint8_t idx = 0) override {
     ValueProducer<T>::output = newValue;
     this->notify();
   }
@@ -32,14 +32,14 @@ class Passthrough : public OneToOneTransform<T> {
     return json;
   }
 
-  virtual JsonObject& get_configuration(JsonBuffer& buf) override final {
+  virtual JsonObject& get_configuration(JsonBuffer& buf) override {
     JsonObject& root = buf.createObject();
     root["sk_path"] = TransformBase::sk_path;
     root["value"] = ValueProducer<T>::output;
     return root;
   }
 
-  virtual bool set_configuration(const JsonObject& config) override final {
+  virtual bool set_configuration(const JsonObject& config) override {
     if (!config.containsKey("sk_path")) {
       return false;
     }
