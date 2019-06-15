0a850216c00a18ecfe93569ae9dacf6050dd2673
Butch <ba58smith@gmail.com>
2019-06-15 14:58:25 -0400

    Update RPM example after refactoring

--- examples/temperature_sender.cpp
+++ examples/temperature_sender.cpp
@@ -1,93 +1,76 @@
 #include <Arduino.h>
 
+//#define SERIAL_DEBUG_DISABLED
+
+#include "sensors/digital_input.h"
 #include "sensesp_app.h"
-#include "transforms/linear.h"
-#include "sensors/analog_input.h"
-
-#include "transforms/analogvoltage.h"
-#include "transforms/voltagedividerR2.h"
-#include "transforms/curveinterpolator.h"
+#include "transforms/frequency.h"
 #include "signalk/signalk_output.h"
-
-
-
-/**
- * A custom transform that takes a resistance value on ohms and returns the estimated
- * temperature in Kelvin.  Sample data in this example were taken from a Westerbeke generator
- * temperature gauge.
- */
-class TemperatureInterpreter : public CurveInterpolator {
-
-    public:
-        TemperatureInterpreter(String config_path="") :
-           CurveInterpolator(NULL, config_path ) {
-
-          // Populate a lookup table tp translate the ohm values returned by
-          // our temperature sender to Kelvin
-          clearSamples();
-//          addSample(CurveInterpolator::Sample(knownOhmValue, knopwnKelvin));
-          addSample(CurveInterpolator::Sample(0, 418.9));
-          addSample(CurveInterpolator::Sample(5, 414.71));
-          addSample(CurveInterpolator::Sample(36, 388.71));
-          addSample(CurveInterpolator::Sample(56, 371.93));
-          addSample(CurveInterpolator::Sample(59, 366.48));
-          addSample(CurveInterpolator::Sample(81, 355.37));
-          addSample(CurveInterpolator::Sample(112, 344.26));
-          addSample(CurveInterpolator::Sample(240, 322.04));
-          addSample(CurveInterpolator::Sample(550, 255.37));
-          addSample(CurveInterpolator::Sample(10000, 237.6));
-
-        }
-};
-
 
 
 // SensESP builds upon the ReactESP framework. Every ReactESP application
 // defines an "app" object vs defining a "main()" method.
-ReactESP app([] () {
-
-  // Some initialization boilerplate when in debug mode...
-  #ifndef SERIAL_DEBUG_DISABLED
+ReactESP app([]() {
+#ifndef SERIAL_DEBUG_DISABLED
   Serial.begin(115200);
 
   // A small arbitrary delay is required to let the
   // serial port catch up
 
-  delay(1000);
+  delay(100);
   Debug.setSerialEnabled(true);
-  #endif
+#endif
 
-  // Create the global SensESPApp() object.
   sensesp_app = new SensESPApp();
 
 
-  // The "SignalK path" identifies this sensor to the SignalK network.
-  const char* sk_path = "electrical.generator.engine.water.temp";
+
+  // The "SignalK path" identifies the output of the sensor to the SignalK network.
+  // If you have multiple sensors connected to your microcontoller (ESP), each one of them
+  // will (probably) have its own SignalK path variable. For example, if you have two
+  // propulsion engines, and you want the RPM of each of them to go to SignalK, you might
+  // have sk_path_leftEngine = "propulsion.left.revolutions"
+  // and  sk_path_rightEngine = "propulsion.right.revolutions"
+  // In this example, there is only one propulsion engine, and its RPM is the only thing
+  // being reported to SignalK.
+  const char* sk_path = "propulsion.left.revolutions";
 
 
+  // The "Configuration path" is combined with "/config" to formulate a URL
+  // used by the RESTful API for retrieving or setting configuration data.
+  // It is ALSO used to specify a path to the SPIFFS file system
+  // where configuration data is saved on the MCU board.  It should
+  // ALWAYS start with a forward slash if specified.  If left blank,
+  // that indicates this sensor or transform does not have any
+  // configuration to save.
+  // Note that if you want to be able to change the sk_path at runtime,
+  // you will need to specify a config_path.
+  const char* config_path = "/sensors/engine_rpm";
 
-  // Connecting a "sender" to the MCU involves using a "voltage divider" circuit. A resistor (R1) is connected
-  // to the voltage output of the board (3.3v or 5v, specified in Vin) to an intersection of two wires.  One
-  // wire connects to the AnalogInput pin of the MCU, and the other wire connects to the sending
-  // unit doing the measurement. The sender is itself a variable resistor that is then connected to ground, forming
-  // the "R2" of a voltage divider circuit. The transform "VoltageDividerR2" can take a known
-  // voltage in, resistance R1, and based on the current voltage read from AnalogInput, compute
-  // the current resistance of the sender (R2).
-  const float Vin = 3.3; // Voltage sent into the voltage divider circuit that includes the analog sender
-  const float R1 = 51.0; // The resistance, in ohms, of the R1 resitor in the analog sender divider circuit
-
-  auto* pAnalogInput = new AnalogInput();
-
-  // Translating the number returned by AnalogInput into a temperature requires several transformations.
-  // Wire them up in sequence...
-  pAnalogInput->connectTo(new AnalogVoltage()) ->
-                connectTo(new VoltageDividerR2(R1, Vin, "/gen/temp/sender")) ->
-                connectTo(new TemperatureInterpreter("/gen/temp/curve")) ->
-                connectTo(new Linear(1.0, 0.0, "/gen/temp/calibrate")) ->
-                connectTo(new SKOutputNumber(sk_path, "/gen/temp/sk"));
+  // These two are necessary until a method is created to synthesize them:
+  const char* config_path_calibrate = "/sensors/engine_rpm/calibrate";
+  const char* config_path_skpath = "/sensors/engine_rpm/sk";
 
 
-  // Start the SensESP application running
+//////////
+// connect a RPM meter. A DigitalInputCounter implements an interrupt
+// to count pulses and reports the readings every read_delay ms
+// (500 in the example). A Frequency
+// transform takes a number of pulses and converts that into
+// a frequency. The sample multiplier converts the 97 tooth
+// tach output into Hz, SK native units.
+const float multiplier = 1.0 / 97.0;
+const uint read_delay = 500;
+
+
+  // Wire it all up by connecting the producer directly to the consumer
+  auto* pSensor = new DigitalInputCounter(D5, INPUT_PULLUP, RISING, read_delay);
+
+  pSensor->connectTo(new Frequency(multiplier, config_path_calibrate))  // connect the output of pSensor to the input of Frequency()
+        ->connectTo(new SKOutputNumber(sk_path, config_path_skpath));   // connect the output of Frequency() to a SignalK Output as a number
+
+
+  // Start the SensESP application running. It constantly monitors the interrupt pin, and every
+  // read_delay ms, it sends the calculated frequency to SignalK.
   sensesp_app->enable();
-
 });
