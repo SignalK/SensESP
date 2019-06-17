950e5d7750218603c84bac1ad65c0dd8d1bf24f4
Brian Smith <ba58smith@gmail.com>
2019-06-17 16:54:35 -0400

    Update RPM example after refactoring (#44)
    * Update RPM example after refactoring
    
    * Change left to main

--- examples/rpm_counter.cpp
+++ examples/rpm_counter.cpp
@@ -25,26 +25,42 @@ ReactESP app([]() {
 
 
 
-  // The "SignalK path" identifies this sensor to the SignalK network.
-  const char* sk_path = "propulsion.left.revolutions";
+  // The "SignalK path" identifies the output of the sensor to the SignalK network.
+  // If you have multiple sensors connected to your microcontoller (ESP), each one of them
+  // will (probably) have its own SignalK path variable. For example, if you have two
+  // propulsion engines, and you want the RPM of each of them to go to SignalK, you might
+  // have sk_path_portEngine = "propulsion.port.revolutions"
+  // and sk_path_starboardEngine = "propulsion.starboard.revolutions"
+  // In this example, there is only one propulsion engine, and its RPM is the only thing
+  // being reported to SignalK.
+  const char* sk_path = "propulsion.main.revolutions";
 
 
   // The "Configuration path" is combined with "/config" to formulate a URL
   // used by the RESTful API for retrieving or setting configuration data.
   // It is ALSO used to specify a path to the SPIFFS file system
-  // where configuration data is saved on the MCU board.  It should
-  // ALWAYS start with a forward slash if specified.  If left blank,
+  // where configuration data is saved on the microcontroller.  It should
+  // ALWAYS start with a forward slash if specified. If left blank,
   // that indicates this sensor or transform does not have any
   // configuration to save.
   // Note that if you want to be able to change the sk_path at runtime,
-  // you will need to specify a config_path.
+  // you will need to specify a configuration path.
+  // As with the SignalK path, if you have multiple configurable sensors
+  // connected to the microcontroller, you will have a configuration path
+  // for each of them, such as config_path_portEngine = "/sensors/portEngine/rpm"
+  // and config_path_starboardEngine = "/sensor/starboardEngine/rpm".
   const char* config_path = "/sensors/engine_rpm";
 
+  // These two are necessary until a method is created to synthesize them. Everything
+  // after "/sensors" in each of these ("/engine_rpm/calibrate" and "/engine_rpm/sk")
+  // is simply a label to display what you're configuring in the Configuration UI. 
+  const char* config_path_calibrate = "/sensors/engine_rpm/calibrate";
+  const char* config_path_skpath = "/sensors/engine_rpm/sk";
 
 
 //////////
-// connect a RPM meter. A DigitalInputCounter counts pulses
-// and reports the readings every read_delay ms
+// connect a RPM meter. A DigitalInputCounter implements an interrupt
+// to count pulses and reports the readings every read_delay ms
 // (500 in the example). A Frequency
 // transform takes a number of pulses and converts that into
 // a frequency. The sample multiplier converts the 97 tooth
@@ -53,23 +69,15 @@ const float multiplier = 1.0 / 97.0;
 const uint read_delay = 500;
 
 
-  // Two ways to wire up sensors and transformations:
+  // Wire it all up by connecting the producer directly to the consumer
+  auto* pSensor = new DigitalInputCounter(D5, INPUT_PULLUP, RISING, read_delay);
 
-  // 1. Connect by specifying data type of Producer's output/Consumer's input
-  /*
-     sensesp_app->connect<int>(
-          new DigitalInputCounter(D5, INPUT_PULLUP, RISING, read_delay),
-          new Frequency(multiplier, config_path)
-     );
-  */
-
-  // 2. Connect the producer directly to the consumer
-  auto* sensor = new DigitalInputCounter(D5, INPUT_PULLUP, RISING, read_delay);
-
-  sensor->connectTo(new Frequency(multiplier, "/sensors/engine_rpm/calibrate"))
-        ->connectTo(new SKOutputNumber(sk_path, "/sensors/engine_rpm/sk"));
+  pSensor->connectTo(new Frequency(multiplier, config_path_calibrate))  // connect the output of pSensor to the input of Frequency()
+        ->connectTo(new SKOutputNumber(sk_path, config_path_skpath));   // connect the output of Frequency() to a SignalK Output as a number
 
 
-  // Start the SensESP application running
+  // Start the SensESP application running. It constantly monitors the interrupt pin, and every
+  // read_delay ms, it sends the calculated frequency to SignalK.
   sensesp_app->enable();
 });
+
