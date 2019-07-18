45bf3f336032d4dde190e36ba0865caf93f9f769
joelkoz <joel.kozikowski@gmail.com>
2019-07-18 19:09:01 -0400

    Remove obsolete files from old version runs

--- docs/class_device.js
+++
@@ -1,4 +0,0 @@
-var class_device =
-[
-    [ "Device", "class_device.html#a170bb69c9bf69109815c18a70a77a875", null ]
-];
--- docs/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['sensesp',['SensESP',['../md___users__joel__workspaces_platformio__sens_e_s_p__r_e_a_d_m_e.html',1,'']]]
-];
