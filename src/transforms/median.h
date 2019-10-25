35a8e8e5043f75cf611ad007ebdae5f1142ed611
joelkoz <joel.kozikowski@gmail.com>
2019-10-25 19:36:13 -0400

    Added "median" to transforms
    Added new "median" transform

---
+++ src/transforms/median.h
@@ -0,0 +1,27 @@
+#ifndef _median_H_
+#define _median_H_
+
+#include <vector>
+
+#include "transforms/transform.h"
+
+/**
+ * Median collects a list of input values of size sampleSize and
+ * outputs the median value (i.e. element number sampleSize / 2 of
+ * a sorted sample list).
+ */
+class Median : public NumericTransform {
+
+ public:
+  Median(int sampleSize = 10, String config_path="");
+  virtual void set_input(float input, uint8_t inputChannel = 0) override;
+  virtual JsonObject& get_configuration(JsonBuffer& buf) override;
+  virtual bool set_configuration(const JsonObject& config) override;
+  virtual String get_config_schema() override;
+
+ private:
+  std::vector<float> buf;
+  int sampleSize;
+};
+
+#endif
