28e224939d112d86d055443c2dafb2528892ff56
kdschmidt1 <56232034+kdschmidt1@users.noreply.github.com>
2019-10-31 10:42:11 +0100

    configuration changed

--- src/sensors/i2c_input.cpp
+++ src/sensors/i2c_input.cpp
@@ -5,7 +5,7 @@
 
 #include "sensesp.h"
 
-int I2CInput::scanI2CAddress(uint8_t address) 
+bool I2CInput::scanI2CAddress(uint8_t address) 
 {
   uint8_t errorCode;
   
@@ -37,11 +37,23 @@ void I2CInput::setupI2C()
 }
 
 
-I2CInput::I2CInput(uint8_t address,String config_path) : NumericSensor() {
+I2CInput::I2CInput(uint8_t given_address,String config_path) : NumericSensor(config_path) {
   className = "I2CInput";
+  Serial.printf("I2C-Input "
+  );
+  address=given_address;
+  load_configuration();
   setupI2C() ;
   if(!scanI2CAddress(address))
-    Serial.printf("Kein I2C Device an Adresse %x", address);
+    {
+      Serial.printf("Kein I2C Device an Adresse %x", address);
+      found=false;
+    }
+    else
+    {
+      found=true;
+    }
+    
 
 }
 
@@ -56,3 +68,32 @@ void I2CInput::enable()
   app.onRepeat(100, [this](){ this->update(); });
 }
 
+JsonObject& I2CInput::get_configuration(JsonBuffer& buf) {
+  JsonObject& root = buf.createObject();
+  root.set("value", output);
+  root.set("address", address);
+  root.set("found", found);
+  return root;
+}
+
+static const char SCHEMA[] PROGMEM = R"({
+    "type": "object",
+    "properties": {
+        "address": { "title": "I2C address", "type": "number" },
+        "found": { "title": "Device found", "type": "boolean", "readOnly": true },
+        "value": { "title": "Last value", "type" : "number", "readOnly": true }
+    }
+  })";
+
+String I2CInput::get_config_schema() {
+  return FPSTR(SCHEMA);
+}
+
+bool I2CInput::set_configuration(const JsonObject& config) {
+  if (!config.containsKey("address")) {
+    return false;
+  }
+  address = config["address"];
+  found== config["found"];
+  return true;
+}
--- src/sensors/i2c_input.h
+++ src/sensors/i2c_input.h
@@ -6,14 +6,21 @@
 class I2CInput : public NumericSensor {
 
 public:
-  int scanI2CAddress(uint8_t address) ;
+    I2CInput(uint8_t given_address,
+                     String config_path="");
+  bool scanI2CAddress(uint8_t address);
+  void enable() override final;
   void scanAllI2C();
   void setupI2C();
-  I2CInput(uint8_t address,String config_path="");
-  void enable() override final;
+  virtual JsonObject& get_configuration(JsonBuffer& buf) override final;
+  virtual bool set_configuration(const JsonObject& config) override final;
+  virtual String get_config_schema() override;
 
 private:
+  bool found = true;
+  uint8_t address;
   void update();
+  void read_value();
 };
 
 
