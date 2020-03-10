12d73711f7d99682dea3ba4dac43b1bef50fd5fe
Butch <ba58smith@gmail.com>
2020-03-10 10:54:48 -0400

    Add configurable read_delay to AnalogInput

--- src/sensors/analog_input.cpp
+++ src/sensors/analog_input.cpp
@@ -4,7 +4,8 @@
 
 #include "sensesp.h"
 
-AnalogInput::AnalogInput() : NumericSensor() {
+AnalogInput::AnalogInput(uint read_delay, String config_path) 
+                 : NumericSensor(config_path), read_delay{read_delay} {
   className = "AnalogInput";
 }
 
@@ -14,5 +15,35 @@ void AnalogInput::update() {
 }
 
 void AnalogInput::enable() {
-  app.onRepeat(100, [this](){ this->update(); });
+  app.onRepeat(read_delay, [this](){ this->update(); });
+}
+
+JsonObject& AnalogInput::get_configuration(JsonBuffer& buf) {
+  JsonObject& root = buf.createObject();
+  root["read_delay"] = read_delay;
+  root["value"] = output;
+  return root;
+  };
+
+  static const char SCHEMA[] PROGMEM = R"###({
+    "type": "object",
+    "properties": {
+        "read_delay": { "title": "Read delay", "type": "number", "description": "Number of milliseconds between each analogRead(A0)" },
+        "value": { "title": "Last value", "type" : "number", "readOnly": true }
+    }
+  })###";
+
+  String AnalogInput::get_config_schema() {
+  return FPSTR(SCHEMA);
+}
+
+bool AnalogInput::set_configuration(const JsonObject& config) {
+  String expected[] = {"read_delay"};
+  for (auto str : expected) {
+    if (!config.containsKey(str)) {
+      return false;
+    }
+  }
+  read_delay = config["read_delay"];
+  return true;
 }
