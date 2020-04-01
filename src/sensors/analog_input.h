bd405747953c795e364857d1d34df2bea05507ca
Brian Smith <ba58smith@gmail.com>
2020-04-01 14:30:48 -0400

    Add configurable read_delay to AnalogInput (#77)
    * Add configurable read_delay to AnalogInput
    
    * forgot load_configuration() in constructor

--- src/sensors/analog_input.h
+++ src/sensors/analog_input.h
@@ -6,10 +6,14 @@
 class AnalogInput : public NumericSensor {
 
 public:
-  AnalogInput();
+  AnalogInput(uint read_delay = 200, String config_path = "");
   void enable() override final;
 
 private:
+  uint read_delay;
+  virtual JsonObject& get_configuration(JsonBuffer& buf) override;
+  virtual bool set_configuration(const JsonObject& config) override;
+  virtual String get_config_schema() override;
   void update();
 };
 
