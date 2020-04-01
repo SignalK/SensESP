f066869027d00bf66dea11e6a382aacca5633b6e
Butch <ba58smith@gmail.com>
2020-04-01 14:21:32 -0400

    Remove a bunch of outdated comments

--- src/sensors/ads1x15.h
+++ src/sensors/ads1x15.h
@@ -45,44 +45,5 @@ class ADS1x15value : public NumericSensor {
 typedef ADS1x15value<ADS1015> ADS1015value;
 typedef ADS1x15value<ADS1115> ADS1115value;
 
-/*
-
-// ADS1015channel is used to read the value of an ADS1015. If you want to read a single channel,
-// make your channel parameter be 0, 1 2 or 3 and readADC_SingleEnded() will be used. If you want
-// to read the difference between two channels, your channel parameter should be:
-// - 10 will use readADC_Differential_0_1()
-// - 23 will use readADC_Differential_2_3()
-class ADS1015channel : public NumericSensor {
-  public:
-    ADS1015channel(ADS1015* ads1015, uint8_t channel = 0, uint read_delay = 200, String config_path="");
-    void enable() override final;
-
-  private:
-    ADS1015* ads1015;
-    uint8_t channel;
-    uint read_delay;
-    virtual JsonObject& get_configuration(JsonBuffer& buf) override;
-    virtual bool set_configuration(const JsonObject& config) override;
-    virtual String get_config_schema() override;
-
-};
-
-// See description of ADS1015channel.
-class ADS1115channel : public NumericSensor {
-  public:
-    ADS1115channel(ADS1115* ads1115, uint8_t channel = 0, uint read_delay = 200, String config_path="");
-    void enable() override final;
-
-  private:
-    ADS1115* ads1115;
-    uint8_t channel;
-    uint read_delay;
-    virtual JsonObject& get_configuration(JsonBuffer& buf) override;
-    virtual bool set_configuration(const JsonObject& config) override;
-    virtual String get_config_schema() override;
-
-};
-
-*/
 
 #endif
