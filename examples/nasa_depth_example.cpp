611116dc6f5e3714d28d2d107c54891c70ca934b
pshotton <phil.shotton@cloudscapesolutions.com>
2020-07-21 18:05:15 +0100

    Issue #139. First cut, to be tested.

---
+++ examples/nasa_depth_example.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+
+//#define SERIAL_DEBUG_DISABLED
+
+#define USE_LIB_WEBSOCKET true
+
+#include <sensors/digital_input.h>
+#include "sensesp_app.h"
+#include "signalk/signalk_output.h"
+#include "wiring_helpers.h"
+#include "sensors/nasa_depth.h"
+
+ReactESP app([] () {
+  #ifndef SERIAL_DEBUG_DISABLED
+  Serial.begin(115200);
+
+  // A small arbitrary delay is required to let the
+  // serial port catch up
+
+  delay(100);
+  Debug.setSerialEnabled(true);
+  #endif
+
+  sensesp_app = new SensESPApp();
+
+  const char* sk_path = "environment.depth.belowTransducer";
+  const char* config_path_skpath = "/depth/sk";
+  const uint read_delay = 500;
+
+  auto* depth = new NasaDepth(D2, D1, read_delay, "/depth");
+  // if your led pin is not LED_BUILTIN then uncomment and set below:
+  // depth->set_led_pin(your_pin);
+      
+  depth->connectTo(new SKOutputNumber(sk_path, config_path_skpath));      
+
+  sensesp_app->enable();
+});
