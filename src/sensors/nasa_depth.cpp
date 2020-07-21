dcbf2245fbe2475780b3d7e1ac47af35250f6853
pshotton <phil.shotton@cloudscapesolutions.com>
2020-07-21 18:05:15 +0100

    Issue #139. First cut, to be tested.

---
+++ src/sensors/nasa_depth.cpp
@@ -0,0 +1,236 @@
+/*
+
+    NASAClipper_I2C_to_SensESP v1.0
+    A sensor for SensESP to decode the NASA Clipper Depth Data
+    
+    Written by Phil Shotton
+    heavily based on code by Peter Holtermann
+    with additions from Victor Klein
+
+    This software is distributed under the GPL v3.0 License
+    
+    
+    At the back of your NASA Clipper should be a round connector
+    with 5 pins, looking roughly like this ASCII art:
+      Pin Location Pin Number
+          | 2nd
+      \ / 4 5
+    - - 1 3
+          
+          O 6
+          
+  Pin 1: SCL
+  Pin 2: GND
+  Pin 3: SDA
+  Pin 4: 12V
+  Pin 5: GND
+
+  For detail refer also to:
+  
+  http://wiki.openseamap.org/wiki/De:NASA_Clipper_Range
+  
+  If you connect SCL, SDA and GND with your arduino
+  and upload this sketch you should get the depth
+  information sent to your local Signal K server.
+
+*/
+#include "nasa_depth.h"
+
+static byte i2C_bytes_read;
+static char i2c_data [11];
+static bool new_data = FALSE;
+
+
+static void I2cDataHandler(int numBytes)
+{
+  byte i = 0;
+  for (i = 0; i <numBytes; i ++)
+  {
+    i2c_data [i] = Wire.read ();
+  }
+  i2C_bytes_read = numBytes;
+  new_data = TRUE;
+}
+
+NasaDepth::NasaDepth(int8_t sda_pin, int8_t scl_pin, uint read_delay, String config_path) :
+    NumericSensor(config_path), sda_pin{sda_pin}, scl_pin{scl_pin}, read_delay{read_delay} {
+  className = "NasaDepth";
+  load_configuration();
+  Wire.begin(sda_pin, scl_pin);
+  Wire.onReceive (&I2cDataHandler);
+}
+
+void NasaDepth::set_led_pin(int8_t pin) {
+  led_pin = pin;
+}
+
+void NasaDepth::enable() {
+  app.onRepeat(read_delay, [this]() {
+    if (new_data) {
+      output = DecodeData();
+      if (output >= 0) {
+        this->notify();
+      }
+    } 
+  });
+}
+
+JsonObject& NasaDepth::get_configuration(JsonBuffer& buf) {
+  JsonObject& root = buf.createObject();
+  root["read_delay"] = read_delay;
+  root["value"] = output;
+  return root;
+};
+
+static const char SCHEMA[] PROGMEM = R"###({
+    "type": "object",
+    "properties": {
+        "read_delay": { "title": "Read delay", "type": "number", "description": "Number of milliseconds between each thermocouple read " },
+        "value": { "title": "Last value", "type" : "number", "readOnly": true }
+    }
+  })###";
+
+String NasaDepth::get_config_schema() { return FPSTR(SCHEMA); }
+
+bool NasaDepth::set_configuration(const JsonObject& config) {
+  String expected[] = {"read_delay"};
+  for (auto str : expected) {
+    if (!config.containsKey(str)) {
+      return false;
+    }
+  }
+  read_delay = config["read_delay"];
+  return true;
+}
+
+// The NASA Clipper sends a 12 byte I2C data packet, this data is directly send to a pcf8566p
+// LCD Driver. The first byte is the address and the write direction, the next eleven bytes is data.
+// The first 5 bytes is a command, the proceeding 6 bytes are data
+// positions and contain the single LCD elements. 
+// Example data {0x7c, 0xce, 0x80,0xe0,0xf8,0x70,0x00,0x00,0x00,0x00,0x00,0x00};
+// addr 0 1 2 3 4 5 6 7 8 9 10
+// com com com com com dta dta dta dta dta dta
+// Example depth: 23.3
+// Digit number: 12.3
+float NasaDepth::DecodeData () 
+{
+  byte i, j;
+  byte digit_tmp0, digit_tmp1, dig1, dig2, dig3, dec_point, index;
+  char output_string [5];
+  float output = -1;
+  new_data = FALSE;
+
+    index = 0;
+    
+    // Copy the rawdata
+    for (j = 0; j <11; j ++) {
+      data [j] = i2c_data [j];
+      i2c_data [j] = 0;
+    }
+    // Check if the first 5 byte (the command) are correct
+    // They seem to stay always the same
+    for (i = 0; i <5; i ++) {
+      output_string [i] = 0;
+      
+      if ((data [i] & 0xFF) == (i2C_predata [i] & 0xFF)) {
+        valid_data = TRUE;
+      }
+      else {
+        valid_data = FALSE;
+        break;
+      }
+    }
+    if ((valid_data) & (i2C_bytes_read == 11)) {
+      // Decode the digits
+      dig1 = 'N';
+      dig2 = 'N';
+      dig3 = 'N';
+      dec_point = 'N';
+  
+       // DIGIT 3 
+      digit_tmp0 = data [6] & digit3_mask [1];
+      for (i = 0; i <10; i ++) {
+        if ((digit3 [i] [1] & 0xFF) == (digit_tmp0 & 0xFF)) {
+          dig3 = '0' + i;
+          break;
+        }
+      }
+      // decimal point
+      if ((data [8] & decpoint_mask [3] & 0xFF) == (0x80)) {
+        dec_point = '.';
+      }
+      
+      // We only consider data good, when the "DEPTH" symbol appears on the LCD
+      if ((data [5] & depth_mask [0] & 0xFF) == (0x01)) {
+        valid_depth = TRUE;
+      }
+      else {
+        valid_depth = FALSE;
+      }
+      
+      
+      // DIGIT 2 
+      digit_tmp0 = data [5] & digit2_mask [0];
+      for (i = 0; i <10; i ++) {
+        if ((digit2 [i] [0] & 0xFF) == (digit_tmp0 & 0xFF)) {        
+          dig2 = '0' + i;
+          break;
+        }
+      }
+      // DIGIT 1
+      digit_tmp0 = data [9] & digit1_mask [4];
+      digit_tmp1 = data [10] & digit1_mask [5];
+      for (i = 0; i <10; i ++) {
+        if (((digit1 [i] [4] & 0xFF) == (digit_tmp0 & 0xFF)) &
+        ((digit1 [i] [5] & 0xFF) == (digit_tmp1 & 0xFF))) {
+          dig1 = '0' + i;
+          break;
+        }
+      }
+      
+      i = 0;
+      // Do we have good data? (FLAG_DEPTH and at least one digit
+      if (((dig1 != 'N') || (dig2 != 'N') || (dig3 != 'N')) && valid_depth) {
+        index = 0;
+        if (dig1 != 'N') {
+          output_string [index] = dig1;
+          index ++;
+        }
+        if (dig2 != 'N') {
+          output_string [index] = dig2;
+          index ++;
+        }
+        if (dec_point != 'N') {
+          output_string [index] = dec_point;
+          index ++;
+        }
+        if (dig3 != 'N') {
+          output_string [index] = dig3;
+          index ++;
+        }
+        output_string [index] = '\0';
+        Serial.println (output_string);
+        sscanf(output_string, "%f", &output);
+
+        // Toggle the LED
+        if (led_on == HIGH) {
+          led_on = LOW;
+          digitalWrite (led_pin, LOW);
+        }
+        else {
+          led_on = HIGH;
+          digitalWrite (led_pin, HIGH);
+        }
+
+      } else {
+        Serial.println ("$ bad data");
+      }
+    }
+    // Get rid of old data
+    for (i = 0; i <11; i ++) {
+      data [i] = 0;
+    }
+    return output;  
+}
+
+ 
---
+++ src/sensors/nasa_depth.h
@@ -0,0 +1,85 @@
+#ifndef _NASA_DEPTH_H_
+#define _NASA_DEPTH_H_
+
+#include "sensesp.h"
+#include "sensor.h"
+#include "Wire.h"
+
+#ifndef FALSE
+#define FALSE 0
+#define TRUE 1
+#endif
+
+class NasaDepth : public NumericSensor {
+  public:
+    NasaDepth(int8_t sda_pin, int8_t scl_pin, uint read_delay, String config_path = "");
+    void enable() override final;
+    void set_led_pin(int8_t pin);
+
+  private:
+    int8_t sda_pin;
+    int8_t scl_pin;
+    boolean led_on = FALSE;
+    byte led_pin = LED_BUILTIN;
+    boolean data_available = FALSE;
+    char data [11];
+    bool valid_data = FALSE;
+    bool valid_depth = FALSE;
+    float DecodeData ();
+
+    uint read_delay;
+    virtual JsonObject& get_configuration(JsonBuffer& buf) override;
+    virtual bool set_configuration(const JsonObject& config) override;
+    virtual String get_config_schema() override;
+
+    char i2C_predata [5] = {0xce, 0x80, 0xe0, 0xf8, 0x70};
+    char depth_mask [6] = {0x01, 0, 0, 0, 0, 0};
+    char decpoint_mask [6] = {0, 0, 0, 0x80, 0x0, 0x0};
+    char meters_mask [6] = {0, 0, 0, 0x40, 0x0, 0x0};
+    char digit3_mask [6] = {0, 0xbf, 0, 0, 0, 0};
+    char digit3 [10] [6] = {// from https://en.wikipedia.org/wiki/Seven-segment_display
+                   {0, 0xbb, 0, 0, 0, 0}, // zero, a, b, c, d, e, f, / g
+                   {0, 0x11, 0, 0, 0, 0}, // one / a, b, c, / d, / e, / f, / g
+                   {0, 0x9e, 0, 0, 0, 0}, // two a, b, / c, d, e, / f, g
+                   {0, 0x97, 0, 0, 0, 0}, // three a, b, c, d, / e, / f, g
+                   {0, 0x35, 0, 0, 0, 0}, // four / a, b, c, / d, / e, f, g
+                   {0, 0xa7, 0, 0, 0, 0}, // five a, / b, c, d, / e, f, g
+                   {0, 0xaf, 0, 0, 0, 0}, // six a, / b, c, d, e, f, g
+                   {0, 0x91, 0, 0, 0, 0}, // seven a, b, c, / d, / e, / f, / g
+                   {0, 0xbf, 0, 0, 0, 0}, // eight a, b, c, d, e, f, g
+                   {0, 0xb7, 0, 0, 0, 0}, // nine a, b, c, d, / e, f, g
+                 };
+                 
+    char digit2_mask [6] = {0xfe, 0, 0, 0, 0, 0};
+    char digit2 [10] [6] = {// from https://en.wikipedia.org/wiki/Seven-segment_display
+                   {0xee, 0, 0, 0, 0, 0}, // zero, a, b, c, d, e, f, / g
+                   {0x44, 0, 0, 0, 0, 0}, // one / a, b, c, / d, / e, / f, / g
+                   {0xb6, 0, 0, 0, 0, 0}, // two a, b, / c, d, e, / f, g
+                   {0xd6, 0, 0, 0, 0, 0}, // three a, b, c, d, / e, / f, g
+                   {0x5c, 0, 0, 0, 0, 0}, // four / a, b, c, / d, / e, f, g
+                   {0xda, 0, 0, 0, 0, 0}, // five a, / b, c, d, / e, f, g
+                   {0xfa, 0, 0, 0, 0, 0}, // six a, / b, c, d, e, f, g
+                   {0x46, 0, 0, 0, 0, 0}, // seven a, b, c, / d, / e, / f, / g
+                   {0xfe, 0, 0, 0, 0, 0}, // eight a, b, c, d, e, f, g
+                   {0xde, 0, 0, 0, 0, 0}, // nine a, b, c, d, / e, f, g
+                 };
+                 
+
+    char digit1_mask [6] = {0, 0, 0, 0, 0x2f, 0xc0};
+    char digit1 [10] [6] = {// from https://en.wikipedia.org/wiki/Seven-segment_display
+                   {0, 0, 0, 0, 0x2e, 0xc0}, // zero, a, b, c, d, e, f, / g
+                   {0, 0, 0, 0, 0x04, 0x40}, // one / a, b, c, / d, / e, / f, / g
+                   {0, 0, 0, 0, 0x27, 0x80}, // two a, b, / c, d, e, / f, g
+                   {0, 0, 0, 0, 0x25, 0xC0}, // three a, b, c, d, / e, / f, g
+                   {0, 0, 0, 0, 0x0d, 0x40}, // four / a, b, c, / d, / e, f, g
+                   {0, 0, 0, 0, 0x29, 0xC0}, // five a, / b, c, d, / e, f, g
+                   {0, 0, 0, 0, 0x2b, 0xC0}, // six a, / b, c, d, e, f, g
+                   {0, 0, 0, 0, 0x24, 0x40}, // seven a, b, c, / d, / e, / f, / g
+                   {0, 0, 0, 0, 0x2f, 0xc0}, // eight a, b, c, d, e, f, g
+                   {0, 0, 0, 0, 0x2d, 0xc0}, // nine a, b, c, d, / e, f, g
+                 };
+
+
+};
+
+#endif
