f275fe161567ed909993619140a7c6854be56e78
Matti Airas <mairas@iki.fi>
2020-10-03 21:29:32 +0300

    Rename Median args

--- src/transforms/median.cpp
+++ src/transforms/median.cpp
@@ -1,18 +1,18 @@
 #include "median.h"
 
-Median::Median(unsigned int sampleSize, String config_path)
-    : NumericTransform(config_path), sampleSize{sampleSize} {
+Median::Median(unsigned int sample_size, String config_path)
+    : NumericTransform(config_path), sample_size{sample_size} {
   load_configuration();
-  buf.reserve(sampleSize);
+  buf.reserve(sample_size);
   buf.clear();
 }
 
 void Median::set_input(float input, uint8_t inputChannel) {
   buf.push_back(input);
-  if (buf.size() >= sampleSize) {
+  if (buf.size() >= sample_size) {
     // Its time to output a value
     sort(buf.begin(), buf.end());
-    int mid = sampleSize / 2;
+    int mid = sample_size / 2;
     output = buf[mid];
     buf.clear();
     notify();
@@ -20,29 +20,29 @@ void Median::set_input(float input, uint8_t inputChannel) {
 }
 
 void Median::get_configuration(JsonObject& root) {
-  root["sampleSize"] = sampleSize;
+  root["sample_size"] = sample_size;
 }
 
 static const char SCHEMA[] PROGMEM = R"({
     "type": "object",
     "properties": {
-        "sampleSize": { "title": "Sample size", "description": "Number of samples to take before outputing a value", "type": "integer" }
+        "sample_size": { "title": "Sample size", "description": "Number of samples to take before outputing a value", "type": "integer" }
     }
   })";
 
 String Median::get_config_schema() { return FPSTR(SCHEMA); }
 
 bool Median::set_configuration(const JsonObject& config) {
-  String expected[] = {"sampleSize"};
+  String expected[] = {"sample_size"};
   for (auto str : expected) {
     if (!config.containsKey(str)) {
       return false;
     }
   }
-  unsigned int sampleSize_new = config["sampleSize"];
-  if (sampleSize != sampleSize_new) {
-    sampleSize = sampleSize_new;
-    buf.reserve(sampleSize);
+  unsigned int sample_size_new = config["sample_size"];
+  if (sample_size != sample_size_new) {
+    sample_size = sample_size_new;
+    buf.reserve(sample_size);
     buf.clear();
   }
   return true;
--- src/transforms/median.h
+++ src/transforms/median.h
@@ -6,22 +6,22 @@
 #include "transforms/transform.h"
 
 /**
- * Median collects a list of input values of size sampleSize and
- * outputs the median value (i.e. element number sampleSize / 2 of
+ * Median collects a list of input values of size sample_size and
+ * outputs the median value (i.e. element number sample_size / 2 of
  * a sorted sample list).
  */
 class Median : public NumericTransform {
 
  public:
-  Median(unsigned int sampleSize = 10, String config_path="");
-  virtual void set_input(float input, uint8_t inputChannel = 0) override;
+  Median(unsigned int sample_size = 10, String config_path="");
+  virtual void set_input(float input, uint8_t input_channel = 0) override;
   virtual void get_configuration(JsonObject& doc) override;
   virtual bool set_configuration(const JsonObject& config) override;
   virtual String get_config_schema() override;
 
  private:
   std::vector<float> buf;
-  unsigned int sampleSize;
+  unsigned int sample_size;
 };
 
 #endif
