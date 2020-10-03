393335b5381020ad711d590b7e106a4e6cc2638a
Matti Airas <mairas@iki.fi>
2020-10-03 20:47:46 +0300

    Rename SKValueListener args

--- src/signalk/signalk_value_listener.h
+++ src/signalk/signalk_value_listener.h
@@ -6,12 +6,12 @@
 #include "system/valueproducer.h"
 #include <ArduinoJson.h>
 ///////////////////
-// SKListener is object that listens for specific value in Signal K, listen_Delay is minimum interval between updates in ms
+// SKListener is object that listens for specific value in Signal K, listen_delay is minimum interval between updates in ms
 ///////////////////
 template <class T>
 class SKValueListener : public SKListener, public ValueProducer<T> {
  public:
-  SKValueListener(String sk_path, int listen_Delay = 1000) : SKListener(sk_path, listen_Delay) 
+  SKValueListener(String sk_path, int listen_delay = 1000) : SKListener(sk_path, listen_delay) 
   {
        if(sk_path == "")
        {
