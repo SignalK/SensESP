6d9f40f316a0e1e9ec6b15f6ed1e6e9880039ba5
Matti Airas <mairas@iki.fi>
2020-10-06 11:49:26 +0300

    Use AnalogReader in AnalogInput

--- src/sensors/analog_input.h
+++ src/sensors/analog_input.h
@@ -1,22 +1,24 @@
 #ifndef _analog_input_H_
 #define _analog_input_H_
 
+#include "analog_reader.h"
 #include "sensor.h"
 
 class AnalogInput : public NumericSensor {
-
-public:
-  AnalogInput(uint8_t pin = A0, uint read_delay = 200, String config_path = "");
+ public:
+  AnalogInput(uint8_t pin = A0, uint read_delay = 200, String config_path = "",
+              float output_scale = 1024.);
   void enable() override final;
 
-private:
+ private:
   uint8_t pin;
   uint read_delay;
+  float output_scale;
+  BaseAnalogReader* analog_reader;
   virtual void get_configuration(JsonObject& doc) override;
   virtual bool set_configuration(const JsonObject& config) override;
   virtual String get_config_schema() override;
   void update();
 };
-
 
 #endif
