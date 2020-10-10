5fe7ed8c5bb5c805deb89d03c0b59959eda670a0
RBerliner <RBerliner@instrumentationassociates.com>
2020-10-09 21:18:56 -0400

    Moved uint read_delay to private and below the declarations for trigger_pin and input_pin. This now compiles for featheresp32.

--- src/sensors/ultrasonic_distance.h
+++ src/sensors/ultrasonic_distance.h
@@ -8,11 +8,11 @@ class UltrasonicDistance : public NumericSensor {
   UltrasonicDistance(int8_t trigger_pin, int8_t input_pin,
                      uint read_delay = 1000, String config_path = "");
   void enable() override final;
-  uint read_delay;
 
  private:
   int8_t trigger_pin;
   int8_t input_pin;
+  uint read_delay;
   virtual void get_configuration(JsonObject& doc) override;
   virtual bool set_configuration(const JsonObject& config) override;
   virtual String get_config_schema() override;
