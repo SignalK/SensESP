2b4d72ea33f2597c487465768e69df511f121e74
RBerliner <RBerliner@instrumentationassociates.com>
2020-10-17 21:27:21 -0400

    Updated SERIAL_DEBUG_DISABLED #ifndef - #endif

--- examples/milone_level_sensor/milone_level_sensor.cpp
+++ examples/milone_level_sensor/milone_level_sensor.cpp
@@ -51,15 +51,8 @@ ReactESP app([]() {
 
 // Some initialization boilerplate when in debug mode...
 #ifndef SERIAL_DEBUG_DISABLED
-  Serial.begin(115200);
-
-  // A small delay and one debugI() are required so that
-  // the serial output displays everything
-  delay(100);
-  Debug.setSerialEnabled(true);
+  SetupSerialDebug(115200);
 #endif
-  delay(100);
-  debugI("Serial debug enabled");
 
   // Create a builder object
   SensESPAppBuilder builder;
