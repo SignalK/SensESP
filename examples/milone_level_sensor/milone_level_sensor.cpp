72965e3b85086a446652147254ee7e9945a66c4b
RBerliner <RBerliner@instrumentationassociates.com>
2020-10-17 20:16:22 -0400

    Converted some vaariables to Google Style naming

--- examples/milone_level_sensor/milone_level_sensor.cpp
+++ examples/milone_level_sensor/milone_level_sensor.cpp
@@ -20,28 +20,28 @@ public:
 
     // Populate a lookup table tp translate the ohm values returned by
     // our level sensor to inches on the sensor scale
-    clearSamples();
-    // addSample(CurveInterpolator::Sample(knownOhmValue, sensorHeight));
-    addSample(CurveInterpolator::Sample(388., 8.5));
-    addSample(CurveInterpolator::Sample(390., 8.3));
-    addSample(CurveInterpolator::Sample(400, 8.15));
-    addSample(CurveInterpolator::Sample(436., 8.0));
-    addSample(CurveInterpolator::Sample(457., 7.75));
-    addSample(CurveInterpolator::Sample(511., 7.25));
-    addSample(CurveInterpolator::Sample(563., 6.75));
-    addSample(CurveInterpolator::Sample(590., 6.3));
-    addSample(CurveInterpolator::Sample(654., 5.8));
-    addSample(CurveInterpolator::Sample(757., 5.15));
-    addSample(CurveInterpolator::Sample(823., 4.6));
-    addSample(CurveInterpolator::Sample(872., 4.2));
-    addSample(CurveInterpolator::Sample(953., 3.6));
-    addSample(CurveInterpolator::Sample(1036., 3.05));
-    addSample(CurveInterpolator::Sample(1112., 2.5));
-    addSample(CurveInterpolator::Sample(1155., 2.1));
-    addSample(CurveInterpolator::Sample(1221., 1.7));
-    addSample(CurveInterpolator::Sample(1329., 1.3));
-    addSample(CurveInterpolator::Sample(1387., 1.0));
-    addSample(CurveInterpolator::Sample(1600., 1.0));
+    clear_samples();
+    // add_sample(CurveInterpolator::Sample(knownOhmValue, sensorHeight));
+    add_sample(CurveInterpolator::Sample(388., 8.5));
+    add_sample(CurveInterpolator::Sample(390., 8.3));
+    add_sample(CurveInterpolator::Sample(400, 8.15));
+    add_sample(CurveInterpolator::Sample(436., 8.0));
+    add_sample(CurveInterpolator::Sample(457., 7.75));
+    add_sample(CurveInterpolator::Sample(511., 7.25));
+    add_sample(CurveInterpolator::Sample(563., 6.75));
+    add_sample(CurveInterpolator::Sample(590., 6.3));
+    add_sample(CurveInterpolator::Sample(654., 5.8));
+    add_sample(CurveInterpolator::Sample(757., 5.15));
+    add_sample(CurveInterpolator::Sample(823., 4.6));
+    add_sample(CurveInterpolator::Sample(872., 4.2));
+    add_sample(CurveInterpolator::Sample(953., 3.6));
+    add_sample(CurveInterpolator::Sample(1036., 3.05));
+    add_sample(CurveInterpolator::Sample(1112., 2.5));
+    add_sample(CurveInterpolator::Sample(1155., 2.1));
+    add_sample(CurveInterpolator::Sample(1221., 1.7));
+    add_sample(CurveInterpolator::Sample(1329., 1.3));
+    add_sample(CurveInterpolator::Sample(1387., 1.0));
+    add_sample(CurveInterpolator::Sample(1600., 1.0));
   }
 };
 
@@ -96,14 +96,14 @@ ReactESP app([]() {
   uint8_t pin = A0;
   uint read_delay = 500;
 
-  auto *pAnalogInput = new AnalogInput(pin, read_delay, kAnalogInConfigPath);
+  auto *analog_input = new AnalogInput(pin, read_delay, kAnalogInConfigPath);
 
   // comment out the following line to suppress the output of the raw ADC measurement values.
-  // pAnalogInput->connectTo(new SKOutputNumber("tanks.freshWater.starboard.rawADC"));
+  // analog_input->connect_to(new SKOutputNumber("tanks.freshWater.starboard.rawADC"));
 
   // Comment out the following 2 lines to suppress the output of the ADC measurement in volts.
-  // pAnalogInput->connectTo(new AnalogVoltage(1.0, 1.0, 0))
-  //             ->connectTo(new SKOutputNumber("tanks.freshWater.starboard.voltsADC"));
+  // analog_input->connect_to(new AnalogVoltage(1.0, 1.0, 0))
+  //             ->connect_to(new SKOutputNumber("tanks.freshWater.starboard.voltsADC"));
 
   // The Milone depth sensor is wired as a voltage divider with Vin connected to the sensor,
   // a variable resistor. The sensor is then connected to R2, a fixed resistor then connected to gnd.
@@ -120,18 +120,18 @@ ReactESP app([]() {
   // and then output the results to the SignalK server.
 
   // Comment out the following 3 lines to suppress the output of the eTape sensor resistance.
-  pAnalogInput->connectTo(new AnalogVoltage(1.0, 1.0, 0.))
-      ->connectTo(new VoltageDividerR1(R2, Vin, "/freshWaterTank_starboard/divider"))
-      ->connectTo(new SKOutputNumber("tanks.freshWater.starboard.R1"));
+  analog_input->connect_to(new AnalogVoltage(1.0, 1.0, 0.))
+      ->connect_to(new VoltageDividerR1(R2, Vin, "/freshWaterTank_starboard/divider"))
+      ->connect_to(new SKOutputNumber("tanks.freshWater.starboard.R1"));
 
   // Use the ETapeInterpolator to output the water level depth in the tank and pass it through
   // the MovingAverage transport before outputting the result.
 
-  pAnalogInput->connectTo(new AnalogVoltage(1.0, 1.0, 0.))
-      ->connectTo(new VoltageDividerR1(R2, Vin, ""))
-      ->connectTo(new ETapeInterpreter(""))
-      ->connectTo(new MovingAverage(samples, scale, "/freshWaterTank_starboard/samples"))
-      ->connectTo(new SKOutputNumber("tanks.freshwater.starboard.currentLevel"));
+  analog_input->connect_to(new AnalogVoltage(1.0, 1.0, 0.))
+      ->connect_to(new VoltageDividerR1(R2, Vin, ""))
+      ->connect_to(new ETapeInterpreter(""))
+      ->connect_to(new MovingAverage(samples, scale, "/freshWaterTank_starboard/samples"))
+      ->connect_to(new SKOutputNumber("tanks.freshwater.starboard.currentLevel"));
 
   // Start the SensESP application running
   sensesp_app->enable();
