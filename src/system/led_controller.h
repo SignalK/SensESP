057b086ffcd829885b85111071c492cb946f71f9
Matti Airas <mairas@iki.fi>
2020-11-08 19:10:02 +0200

    Reformat led_controller.h

--- src/system/led_controller.h
+++ src/system/led_controller.h
@@ -28,11 +28,12 @@ class LedController : public VisualOutputController {
 
   // ValueConsumer interface for ValueConsumer<WifiState> (Networking object
   // state updates)
-  virtual void set_input(WifiState new_value, uint8_t input_channel = 0) override;
+  virtual void set_input(WifiState new_value,
+                         uint8_t input_channel = 0) override;
   // ValueConsumer interface for ValueConsumer<WSConnectionState>
   // (WSClient object state updates)
   virtual void set_input(WSConnectionState new_value,
-                 uint8_t input_channel = 0) override;
+                         uint8_t input_channel = 0) override;
   // ValueConsumer interface for ValueConsumer<int> (delta count producer
   // updates)
   virtual void set_input(int new_value, uint8_t input_channel = 0) override;
