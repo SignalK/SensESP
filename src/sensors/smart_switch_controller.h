fb90f8fe307521beb228b65a9f50d0665b36aee1
joelkoz <joel.kozikowski@gmail.com>
2020-11-21 16:10:39 -0500

    Clarified behavior of smart_switch_controller and String inputs
    Added toggle behavior to docs in relationship to incoming String values.

--- src/sensors/smart_switch_controller.h
+++ src/sensors/smart_switch_controller.h
@@ -17,7 +17,11 @@
  * The incoming boolean values, on the other hand, are assumed to be
  * state setting commands (i.e. turn on or off), so those are used
  * verbatim and forwarded. Incoming string values can be one of 
- * many various "truth" values (such as "on" or "true")
+ * many various "truth" values (such as "on" or "off"). Strings
+ * that are valid truth values (e.g. "on", "true", "1", "off", "false", etc)
+ * will set the switch accordingly. Strings that are NOT valid truth values
+ * will cause the switch state to toggle. Thus, incoming strings like
+ * "toggle" or "default" will toggle the current state and output the results.
  * @see SmartSwitch
  * @see TextToTruth
  */
