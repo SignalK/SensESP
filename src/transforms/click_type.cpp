ec4b6b29f2da2047a3c04de9d3d6e2b4ce129e7b
joelkoz <joel.kozikowski@gmail.com>
2020-11-27 18:30:34 -0500

    Refactored ClickType, removed Button from this PR

--- src/transforms/click_type.cpp
+++ src/transforms/click_type.cpp
@@ -3,96 +3,135 @@
 
 ClickType::ClickType(String config_path, long long_click_delay, long double_click_interval, long ultra_long_click_delay) :
    Transform<bool, ClickTypes>(config_path),
-    press_started{-1},
-    press_released{0},
     click_count{0},
     long_click_delay{long_click_delay},
-    double_click_interval{double_click_interval},
     ultra_long_click_delay{ultra_long_click_delay},
-    queued_report{NULL} {
-   load_configuration();      
+    double_click_interval{double_click_interval},
+    delayed_click_report{NULL} {
+   load_configuration();
 }
+
 
 
 void ClickType::set_input(bool input, uint8_t inputChannel) {
+
   if (input) {
-     // Button press has started
-     if (press_started == -1) {
-        // This is a change in status that we have not yet processed. Process it,
-        // as long as it doesn't come quickly after the last button release...
-        long unpress_interval = millis() - press_released;
-        press_started = millis();
-        if (unpress_interval <= double_click_interval) {
-          if (queued_report != NULL) {
-              queued_report->remove();
-              queued_report = NULL;
-              debugD("ClickType received PRESS: double click detected. Removed queued SingleClick");
-          }
-          click_count++;
-        }
-        else {
-          debugD("ClickType received PRESS (millis: %ld, last press interval: %ld)", millis(), unpress_interval);
-        }
-     }
-     else if ((long)(millis() - press_started) > ultra_long_click_delay) {
-        on_ultra_long_click("PRESS");
-     }
-     else {
-       debugW("ClickType ignoring PRESS while PRESS already started (millis=%ld)", millis());
-     }
+    on_button_press();
   }
   else {
-     // Button has been released
-     if (press_started != -1) {
-        // Finish processing the button press...
-        long press_interval = millis() - press_started;
-        press_started = -1;
-        press_released = millis();
-        if (press_interval >= this->ultra_long_click_delay) {
-            on_ultra_long_click("UNPRESSED");
-            this->press_completed();
-        }
-        else if (press_interval >= this->long_click_delay) {
-            debugD("ClickType UNPRESSED with LongSingleClick (millis: %ld, press interval %ld)", millis(), press_interval);
-            this->emit(ClickTypes::LongSingleClick);
-            this->press_completed();
-        }
-        else if (this->click_count >= 1) {
-          // We have just ended a double click.  Sent it immediately...
-          debugD("ClickType UNPRESSED with DoubleClick (millis: %ld, press interval %ld)", millis(), press_interval);
-          this->emit(ClickTypes::DoubleClick);
-          this->press_completed();
-        }
-        else {
-          // This is the end of the first normal click.  Queue up a send of a single click,
-          // but delay it in case another click comes in prior to the double_click_interval...
-          long ms = millis();
-          queued_report = app.onDelay(double_click_interval+20, [this, press_interval, ms]() {
-              debugD("ClickType UNPRESSED with SingleClick (millis: %ld, queue time: %ld, press interval %ld)", millis(), ms, press_interval);
-              this->emit(ClickTypes::SingleClick);
-              this->press_completed();
-          });
-        }
-     }
-     else {
-       press_released = millis();
-       debugW("ClickType detected UNPRESS with no pending PRESS (millis=%ld)", millis());
-     }
+    on_button_release();
   }
 }
 
+bool ClickType::is_click(ClickTypes value) {
+   return (value != ClickTypes::ButtonPress && value != ClickTypes::ButtonRelease);
+}
 
-void ClickType::press_completed() {
+
+void ClickType::on_button_press() {
+
+    debugD("ClickType received PRESS (millis: %ld, last press interval: %ld)", millis(), (long)press_released);
+
+    if (click_count == 0) {
+      // This is a new, isolated "click" that we have not yet processed.
+      click_count++;
+      press_started = 0;
+    }
+    else {
+      // One or more presses is already in progress...
+
+      if (press_started > ultra_long_click_delay) {
+          // The button down is the second one reported in a row without a button release
+          // and the press is now long enough to qualify as an "ultra long click"
+          on_ultra_long_click("PRESS");
+      }
+      else if (press_released <= double_click_interval) {
+        // This is the start of a second click to come in prior to the expiration of
+        // the double_click_interval.  Remove any "SingleClick" report that may
+        // have been queued up....
+        if (delayed_click_report != NULL) {
+            delayed_click_report->remove();
+            delayed_click_report = NULL;
+            debugD("ClickType received PRESS: double click detected. Removed queued SingleClick");
+        }
+      }
+
+    }
+
+    this->emit(ClickTypes::ButtonPress);
+
+}
+
+
+void ClickType::on_button_release() {
+
+     if (click_count > 0) {
+        // This is the "release" of a click we are tracking...
+        if (press_started >= this->ultra_long_click_delay) {
+            on_ultra_long_click("UNPRESSED");
+            this->on_click_completed();
+        }
+        else if (press_started >= this->long_click_delay) {
+            debugD("ClickType UNPRESSED with LongSingleClick (millis: %ld, press interval %ld)", millis(), (long)press_started);
+            this->emitDelayed(ClickTypes::LongSingleClick);
+            this->on_click_completed();
+        }
+        else if (this->click_count > 1) {
+          // We have just ended a double click.  Sent it immediately...
+          debugD("ClickType UNPRESSED with DoubleClick (millis: %ld, press interval %ld)", millis(), (long)press_started);
+          this->emitDelayed(ClickTypes::DoubleClick);
+          this->on_click_completed();
+        }
+        else {
+          // This is the end of a potential single click.  Queue up the send of a SingleClick report,
+          // but delay it in case another click comes in prior to the double_click_interval, which would
+          // turn this click into a DoubleClick
+          unsigned long time_of_event = millis();
+          long press_interval = (long)press_started;
+          delayed_click_report = app.onDelay(double_click_interval+20, [this, press_interval, time_of_event]() {
+              debugD("ClickType UNPRESSED with SingleClick (millis: %ld, queue time: %ld, press interval %ld)", millis(), time_of_event, press_interval);
+              this->emit(ClickTypes::SingleClick);
+              this->on_click_completed();
+          });
+        }
+
+        press_released = 0;
+        this->emit(ClickTypes::ButtonRelease);
+
+     }
+     else {
+       // A press release with no initial press should happen only when
+       // an UltraLongClick has already been sent, or the producer
+       // is feeding us weird values...
+       press_released = 0;
+       debugW("ClickType detected UNPRESS with no pending PRESS (millis=%ld)", millis());
+     }
+  
+}
+
+
+
+void ClickType::emitDelayed(ClickTypes value) {
+   app.onDelay(5, [this, value]() {
+     this->emit(value);
+   });
+}
+
+
+void ClickType::on_click_completed() {
     this->click_count = 0;
-    queued_report = NULL;
-    press_started = -1;
+    delayed_click_report = NULL;
+    press_started = 0;
     press_released = 0;
 }
 
+
 void ClickType::on_ultra_long_click(const char* keyType) {
     debugD("ClickType %s with UltraLongSingleClick (millis: %ld, press interval %ld)", keyType, millis(), millis() - press_started);
-    this->emit(ClickTypes::UltraLongSingleClick);
+    this->emitDelayed(ClickTypes::UltraLongSingleClick);
+    on_click_completed();
 }
+
 
 void ClickType::get_configuration(JsonObject& root) {
   root["long_click_delay"] = long_click_delay;
--- src/transforms/click_type.h
+++ src/transforms/click_type.h
@@ -1,13 +1,15 @@
 #ifndef _click_types_H_
 #define _click_types_H_
 
+#include <elapsedMillis.h>
+
 #include "transforms/transform.h"
 
 
 /**
- * ClickTypes defines the types of clicks a ClickType transform can detect
+ * ClickTypes defines the types of clicks and button presses a ClickType transform can detect
  */
-enum class ClickTypes { SingleClick, LongSingleClick, UltraLongSingleClick, DoubleClick };
+enum class ClickTypes { ButtonPress, ButtonRelease, SingleClick, LongSingleClick, UltraLongSingleClick, DoubleClick };
 
 
 /**
@@ -37,24 +39,86 @@ class ClickType : public Transform<bool, ClickTypes> {
        *  is immediately emitted. This value should be longer than long_click_delay
        * 
        */
-      ClickType(String config_path = "", long long_click_delay = 1300, long double_click_interval = 350, long ultra_long_click_delay = 5000);
+      ClickType(String config_path = "", long long_click_delay = 1300, long double_click_interval = 400, long ultra_long_click_delay = 5000);
+
+      /**
+       * Returns TRUE if the specified value is one of the higher level click interpretations
+       * like SingleClick or UltraLongDoubleClick. It returns FALSE if the value is
+       * ButtonPress or ButtonRelease
+       */
+      static bool is_click(ClickTypes value);
+
       virtual void set_input(bool input, uint8_t input_channel = 0) override;
       virtual void get_configuration(JsonObject& doc) override;
       virtual bool set_configuration(const JsonObject& config) override;
       virtual String get_config_schema() override;
 
     protected:
-      long press_started;
-      long press_released;
+      /// A counter to specify how many clicks are currently being
+      /// processed. Used to distinguish between single and double
+      /// clicks.
       int click_count;
+
+
+      // User Configuration variables...
+
+      /// How many milliseconds a button is pressed to distinguish between
+      /// a normal SingleClick and a LongSingleClick
       long long_click_delay;
-      long double_click_interval;
+
+      /// How many milliseoncs a button is pressed to distinguish between
+      /// a normal SingleClick and an UltraLongSingleClick
       long ultra_long_click_delay;
-      DelayReaction* queued_report;
 
-      void press_completed();
+      /// The maximum number of milliseconds that can pass before two
+      /// clicks in a row are combined into a single DoubleClick
+      long double_click_interval;
 
+
+      /// Timmer to time button presses
+      elapsedMillis press_started;
+
+
+      /// Timer to time interval between button releases
+      elapsedMillis press_released;
+
+
+      /// Holds a delayed "SingleClick" report that we can pull back
+      /// if the second click of a double click comes through. This
+      /// value will be NULL if no click report is currently
+      /// queued up.
+      DelayReaction* delayed_click_report;
+
+
+      /// Processes incoming values that represent a "ButonPress" event
+      void on_button_press();
+
+
+      /// Processes incoming value that represent a "ButtonRelease" event
+      void on_button_release();
+
+
+      /// Processes an ultra long click. Unlike the other click types,
+      /// ultra long clicks are executed as soon as they pass the
+      /// "ultra long"
       void on_ultra_long_click(const char* keyType);
+
+
+
+      /**
+       * Resets click tracking variables after a press has been released so the
+       * next button press can be processed.
+       */
+      void on_click_completed();
+
+
+      /**
+       * Emits the specified value after a 5 millisecond delay. This allows
+       * translated click types like SingleClick and DoubleClick to be
+       * sent but delays its processing so the ClickTyes::ButtonReleased
+       * can propogate through the system.
+       */
+      void emitDelayed(ClickTypes value);
 };
 
 #endif
