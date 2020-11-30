4a3aaf06f386db9152882c80547589334945ade3
ba58smith <ba58smith@gmail.com>
2020-11-30 11:37:21 -0500

    Change debounce logic

--- src/transforms/debounce.cpp
+++ src/transforms/debounce.cpp
@@ -1,14 +1,48 @@
 #include "debounce.h"
 
-Debounce::Debounce(int ms_min_delay, String config_path)
-    : BooleanTransform(config_path), ms_min_delay{ms_min_delay} {
-  last_time = millis();
+template<class T>
+Debounce<T>::Debounce(int ms_min_delay, String config_path)
+    : SymmetricTransform<T>(config_path), ms_min_delay{ms_min_delay} {
+      interrupt_timer = 0;
 }
 
-void Debounce::set_input(bool newValue, uint8_t inputChannel) {
-  int elapsed = millis() - last_time;
-  if (newValue != output || elapsed > ms_min_delay) {
-    last_time = millis();
+template<class T>
+void Debounce<T>::set_input(T newValue, uint8_t inputChannel) {
+  if (interrupt_timer > ms_min_delay) {
+    interrupt_timer = 0;
     this->emit(newValue);
   }
 }
+
+template<class T>
+void Debounce<T>::get_configuration(JsonObject& root) {
+  root["min_delay"] = ms_min_delay;
+}
+
+static const char SCHEMA[] PROGMEM = R"###({
+    "type": "object",
+    "properties": {
+        "min_delay": { "title": "Minimum delay", "type": "number", "description": "The minimum time in ms between inputs for output to happen" }
+    }
+  })###";
+
+template<class T>
+String Debounce<T>::get_config_schema() { return FPSTR(SCHEMA); }
+
+template<class T>
+bool Debounce<T>::set_configuration(const JsonObject& config) {
+  String expected[] = {"min_delay"};
+  for (auto str : expected) {
+    if (!config.containsKey(str)) {
+      return false;
+    }
+  }
+  ms_min_delay = config["min_delay"];
+  return true;
+}
+
+// define all possible instances of a Debounce
+template class Debounce<bool>;
+template class Debounce<int>;
+template class Debounce<float>;
+template class Debounce<String>;
--- src/transforms/debounce.h
+++ src/transforms/debounce.h
@@ -2,21 +2,37 @@
 #define debounce_H
 
 #include "transforms/transform.h"
+#include <elapsedMillis.h>
 
 /**
- * Debounce is a boolean passthrough transform that will only
- * accept value changes that are sufficiently spaced apart to
- * be passed on to the next consumer.
+ * @brief Debounce is a passthrough transform that will output a value only when
+ * there is ms_min_delay milliseconds between inputs.
+ *
+ * @tparam T The type of value being passed through Debounce.
+ *
+ * @param ms_min_delay The minimum amount of time that must have passed since
+ * the previous input in order for the output to occur.
+ *
+ * @param config_path The path for configuring ms_min_delay with the Config UI.
  */
-class Debounce : public BooleanTransform {
+template<class T>
+class Debounce : public SymmetricTransform<T> {
  public:
   Debounce(int ms_min_delay = 200, String config_path = "");
 
-  virtual void set_input(bool new_value, uint8_t input_channel = 0) override;
+  virtual void set_input(T new_value, uint8_t input_channel = 0) override;
 
- protected:
-  unsigned long last_time;
+ private:
+  elapsedMillis interrupt_timer;
   int ms_min_delay;
+  virtual void get_configuration(JsonObject& doc) override;
+  virtual bool set_configuration(const JsonObject& config) override;
+  virtual String get_config_schema() override;
 };
+
+typedef Debounce<bool> DebounceBool;
+typedef Debounce<int> DebounceInt;
+typedef Debounce<float> DebounceFloat;
+typedef Debounce<String> DebounceString;
 
 #endif
