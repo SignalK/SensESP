04493a3f414a1dfff6bd8b28fdba3f8b3ecab436
joelkoz <joel.kozikowski@gmail.com>
2020-12-04 17:25:49 -0500

    Initial commit of Typecast

---
+++ src/transforms/typecast.cpp
@@ -0,0 +1,9 @@
+#include "transforms/typecast.h"
+
+BoolToInt::BoolToInt() : Typecast<bool, int>([](bool in) -> int { return in ? 1 : 0; })
+{
+}
+
+IntToBool::IntToBool() : Typecast<int, bool>([](int in) -> int { return in != 0; })
+{
+}
