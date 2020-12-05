a5392d727c239b824d342bd741072c1632a33eb9
joelkoz <joel.kozikowski@gmail.com>
2020-12-05 16:39:58 -0500

    Use LambdaTransform and a templated default lambda

--- src/transforms/typecast.cpp
+++ src/transforms/typecast.cpp
@@ -1,23 +1,6 @@
 #include "transforms/typecast.h"
 #include <math.h>
 
-BoolToInt::BoolToInt() : Typecast<bool, int>([](bool in) -> int { return in ? 1 : 0; })
-{
-}
-
-IntToBool::IntToBool() : Typecast<int, bool>([](int in) -> bool { return in != 0; })
-{
-}
-
-IntToFloat::IntToFloat() : Typecast<int, float>([](int in) -> float { return (float)in; })
-{
-}
-
-FloatToInt::FloatToInt() : Typecast<float, int>([](float in) -> int { return (int)in; })
-{
-}
-
-
 RoundToInt::RoundToInt() : Typecast<float, int>([](float in) -> int { return (int)round(in); })
 {
 }
