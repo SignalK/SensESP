d0df8f6e5194173114708444bf18eabfd73e7f62
ba58smith <ba58smith@gmail.com>
2020-12-19 10:27:26 -0500

    Ultrasonic: remove value from Config UI

--- src/sensors/ultrasonic_distance.cpp
+++ src/sensors/ultrasonic_distance.cpp
@@ -28,14 +28,12 @@ void UltrasonicDistance::enable() {
 
 void UltrasonicDistance::get_configuration(JsonObject& root) {
   root["read_delay"] = read_delay_;
-  root["value"] = output;
 };
 
 static const char SCHEMA[] PROGMEM = R"###({
     "type": "object",
     "properties": {
-        "read_delay": { "title": "Read delay", "type": "number", "description": "Number of milliseconds between each thermocouple read " },
-        "value": { "title": "Last value", "type" : "number", "readOnly": true }
+        "read_delay": { "title": "Read delay", "type": "number", "description": "Number of milliseconds between each thermocouple read " }
     }
   })###";
 
