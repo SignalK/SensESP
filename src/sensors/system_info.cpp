028e256c2ce373ead702d52b1d3699c221203eb0
ba58smith <ba58smith@gmail.com>
2020-12-19 10:01:35 -0500

    SystemInfo: Doxygen, data member names

--- src/sensors/system_info.cpp
+++ src/sensors/system_info.cpp
@@ -3,27 +3,27 @@
 #include "Arduino.h"
 #include "sensesp.h"
 
-void SystemHz::tick() { tick_count++; }
+void SystemHz::tick() { tick_count_++; }
 
 void SystemHz::update() {
   uint32_t cur_millis = millis();
-  uint32_t elapsed = cur_millis - prev_millis;
+  uint32_t elapsed = cur_millis - prev_millis_;
 
   // getting sporadic divide by 0 exceptions, no harm in skipping a loop.
   if (elapsed == 0) {
     return;
   }
 
-  output = (tick_count * 1000) / elapsed;
+  output = (tick_count_ * 1000) / elapsed;
 
-  tick_count = 0;
-  prev_millis = cur_millis;
+  tick_count_ = 0;
+  prev_millis_ = cur_millis;
 
   this->notify();
 }
 
 void SystemHz::enable() {
-  prev_millis = millis();
+  prev_millis_ = millis();
 
   app.onTick([this]() { this->tick(); });
   app.onRepeat(1000, [this]() { this->update(); });
