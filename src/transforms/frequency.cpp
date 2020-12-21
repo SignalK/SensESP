a25402b20a516d9f3f7665fb5ee122cbc5319cb2
ba58smith <ba58smith@gmail.com>
2020-12-21 08:49:11 -0500

    Frequency: data member names, change k to multiplier

--- src/transforms/frequency.cpp
+++ src/transforms/frequency.cpp
@@ -2,29 +2,29 @@
 
 // Frequency
 
-Frequency::Frequency(float k, String config_path)
-    : Transform<int, float>(config_path), k{k} {
+Frequency::Frequency(float multiplier, String config_path)
+    : Transform<int, float>(config_path), multiplier_{multiplier} {
   load_configuration();
 }
 
-void Frequency::enable() { last_update = millis(); }
+void Frequency::enable() { last_update_ = millis(); }
 
 void Frequency::set_input(int input, uint8_t inputChannel) {
   unsigned long cur_millis = millis();
-  unsigned long elapsed_millis = cur_millis - last_update;
-  last_update = cur_millis;
-  this->emit(k * input / (elapsed_millis / 1000.));
+  unsigned long elapsed_millis = cur_millis - last_update_;
+  last_update_ = cur_millis;
+  this->emit(multiplier_ * input / (elapsed_millis / 1000.));
 }
 
 void Frequency::get_configuration(JsonObject& root) {
-  root["k"] = k;
+  root["multiplier"] = multiplier_;
   root["value"] = output;
 }
 
 static const char SCHEMA[] PROGMEM = R"###({
     "type": "object",
     "properties": {
-        "k": { "title": "Multiplier", "type": "number" },
+        "multiplier": { "title": "Multiplier", "type": "number" },
         "value": { "title": "Last value", "type" : "number", "readOnly": true }
     }
   })###";
@@ -32,12 +32,12 @@ static const char SCHEMA[] PROGMEM = R"###({
 String Frequency::get_config_schema() { return FPSTR(SCHEMA); }
 
 bool Frequency::set_configuration(const JsonObject& config) {
-  String expected[] = {"k"};
+  String expected[] = {"multiplier"};
   for (auto str : expected) {
     if (!config.containsKey(str)) {
       return false;
     }
   }
-  k = config["k"];
+  multiplier_ = config["multiplier"];
   return true;
 }
