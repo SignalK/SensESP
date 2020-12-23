fb9087a1b5ef618ffc63e7eb255440843355ec74
ba58smith <ba58smith@gmail.com>
2020-12-22 20:10:23 -0500

    RepeatReport: remove value from config output

--- src/transforms/repeat_report.cpp
+++ src/transforms/repeat_report.cpp
@@ -20,14 +20,12 @@ void RepeatReport<T>::enable() {
 template <typename T>
 void RepeatReport<T>::get_configuration(JsonObject& root) {
   root["max_silence_interval"] = max_silence_interval_;
-  root["value"] = this->output;
 }
 
 static const char SCHEMA[] PROGMEM = R"###({
     "type": "object",
     "properties": {
-        "max_silence_interval": { "title": "Max ms interval until value repeated", "type": "number" },
-        "value": { "title": "Last value", "type" : "number", "readOnly": true }
+        "max_silence_interval": { "title": "Max ms interval until value repeated", "type": "number" }
     }
   })###";
 
