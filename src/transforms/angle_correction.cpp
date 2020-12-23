3e572b9be9f09dc95f4a70ec46a1d985d93b3d80
ba58smith <ba58smith@gmail.com>
2020-12-23 18:46:10 -0500

    Revert "AngleCorrection: remove value from config display"
    This reverts commit dc8dffc197e3e5ab282f6a38a77e5b96d234343d.
    
    Two different commits changed the data member names.

--- src/transforms/angle_correction.cpp
+++ src/transforms/angle_correction.cpp
@@ -4,32 +4,34 @@
 
 AngleCorrection::AngleCorrection(float offset, float min_angle,
                                  String config_path)
-    : NumericTransform(config_path), offset_{offset}, min_angle_{min_angle} {
+    : NumericTransform(config_path), offset{offset}, min_angle{min_angle} {
   load_configuration();
 }
 
 void AngleCorrection::set_input(float input, uint8_t inputChannel) {
   // first the correction
-  float x = input + offset_;
+  float x = input + offset;
 
   // then wrap around the values
-  x = fmod(x - min_angle_, 2 * M_PI);
+  x = fmod(x - min_angle, 2 * M_PI);
   if (x < 0) {
     x += 2 * M_PI;
   }
-  this->emit(x + min_angle_);
+  this->emit(x + min_angle);
 }
 
 void AngleCorrection::get_configuration(JsonObject& root) {
-  root["offset"] = offset_;
-  root["min_angle"] = min_angle_;
+  root["offset"] = offset;
+  root["min_angle"] = min_angle;
+  root["value"] = output;
 }
 
 static const char SCHEMA[] PROGMEM = R"###({
     "type": "object",
     "properties": {
         "offset": { "title": "Constant offset", "description": "Value to be added, in radians", "type": "number" },
-        "min_angle": { "title": "Minimum angle value", "description": "If you have output between -pi and pi, use -3.14159265, otherwise use 0.", "type": "number" }
+        "min_angle": { "title": "Minimum angle value", "description": "If you have output between -pi and pi, use -3.14159265, otherwise use 0.", "type": "number" },
+        "value": { "title": "Last value", "type" : "number", "readOnly": true }
     }
   })###";
 
@@ -42,7 +44,7 @@ bool AngleCorrection::set_configuration(const JsonObject& config) {
       return false;
     }
   }
-  offset_ = config["offset"];
-  min_angle_ = config["min_angle"];
+  offset = config["offset"];
+  min_angle = config["min_angle"];
   return true;
 }
