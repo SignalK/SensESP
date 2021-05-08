70e3857c1ba8c8fa62242f95b72d2e5b4030fdb1
ba58smith <ba58smith@gmail.com>
2021-05-07 22:13:58 -0400

    Remove lines that were previously commented-out
    PR #417 just commented out some lines, but they need to be gone. This PR deletes them.

--- src/net/networking.cpp
+++ src/net/networking.cpp
@@ -71,9 +71,6 @@ void Networking::setup_saved_ssid() {
   this->emit(WifiState::kWifiDisconnected);
   setup_wifi_callbacks();
 
-  // this is a workaround for setting the DHCP hostname, suggested in
-  // https://github.com/espressif/arduino-esp32/issues/2537
-  // WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE);
 #ifdef ESP32
   WiFi.setHostname(hostname->get().c_str());
 #elif defined(ESP8266)
@@ -122,9 +119,6 @@ void Networking::setup_wifi_manager() {
 
   this->emit(WifiState::kWifiManagerActivated);
 
-  // this is a workaround for setting the DHCP hostname, suggested in
-  // https://github.com/espressif/arduino-esp32/issues/2537
-  // WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE);
 #ifdef ESP32
   WiFi.setHostname(hostname->get().c_str());
 #elif defined(ESP8266)
