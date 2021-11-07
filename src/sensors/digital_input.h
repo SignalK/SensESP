c2bc38371b4d4e67856fc0cf2cc3fb14b1a7bdec
Matti Airas <mairas@iki.fi>
2021-11-07 19:14:16 +0200

    Refactor the digital input classes
    DigitalInput inheritance was wonky and has been cleaned. The
    base class no longer inherits from the Sensor base class, and the
    member variables that weren't used by all inherited classes have
    been removed.
    
    DigitalInputChange implementation has been simplified. It now triggers
    its output every tick.

--- src/sensors/digital_input.h
+++ src/sensors/digital_input.h
@@ -12,28 +12,24 @@ namespace sensesp {
  *
  * @param pin_mode Will be INPUT or INPUT_PULLUP
  *
- * @param interrupt_type Will be RISING, FALLING, or CHANGE
- *
- * @param config_path The path to configuring read_delay in the Config UI
- *
  * */
-class DigitalInput : public Sensor {
+class DigitalInput {
  public:
-  DigitalInput(uint8_t pin, int pin_mode, int interrupt_type,
-               String config_path = "");
+  DigitalInput(uint8_t pin, int pin_mode) : pin_{pin} {
+    pinMode(pin_, pin_mode);
+  };
 
  protected:
   uint8_t pin_;
-  int interrupt_type_;
 };
 
 /**
- * @brief DigitalInputState reports the state of a digital pin every read_delay
- * ms.
+ * @brief DigitalInputState polls the state of an input pin every read_delay ms.
  *
- * Formerly called DigitalInputValue.
+ * It is useful for sampling the state of an input at constant time intervals
+ * when you are not interested in rapid changes within those time intervals.
  *
- * It's useful for monitoring a device (such as a bilge pump) that should (or
+ * You can use it for monitoring a device (such as a bilge pump) that should (or
  * shouldn't) normally be on (or off). If you use the Signal K to InfluxDb
  * Plugin and Grafana, you can easily see the frequency and duration of the
  * device being on and off.
@@ -42,20 +38,25 @@ class DigitalInput : public Sensor {
  *
  * @param pin_mode Will be INPUT or INPUT_PULLUP.
  *
- * @param interrupt_type Will be RISING, FALLING, or CHANGE, but it doesn't
- * matter which one you use, as this Class doesn't use an interrupt.
- *
  * @param read_delay How often you want to read the pin, in ms.
  *
  * @param config_path The path to configuring read_delay in the Config UI.
  *
  * */
-class DigitalInputState : public DigitalInput, public BoolProducer {
+class DigitalInputState : public DigitalInput, public SensorT<bool> {
  public:
-  DigitalInputState(uint8_t pin, int pin_mode, int interrupt_type,
-                    int read_delay = 1000, String config_path = "");
+  DigitalInputState(uint8_t pin, int pin_mode, int read_delay = 1000,
+                    String config_path = "")
+      : DigitalInput{pin, pin_mode},
+        SensorT<bool>(config_path),
+        read_delay_{read_delay},
+        triggered_{false} {
+    load_configuration();
+  }
 
-  virtual void start() override final;
+  virtual void start() override final {
+    ReactESP::app->onRepeat(read_delay_, [this]() { emit(digitalRead(pin_)); });
+  }
 
  private:
   int read_delay_;
@@ -64,10 +65,6 @@ class DigitalInputState : public DigitalInput, public BoolProducer {
   virtual bool set_configuration(const JsonObject& config) override;
   virtual String get_config_schema() override;
 };
-
-// FIXME: Uncomment the following once the PIO Xtensa toolchain is updated
-// [[deprecated("Use DigitalInputState instead.")]]
-typedef DigitalInputState DigitalInputValue;
 
 /**
  * @brief DigitalInputCounter counts interrupts and reports the count every
@@ -87,14 +84,30 @@ typedef DigitalInputState DigitalInputValue;
  * @param config_path The path to configuring read_delay in the Config UI
  *
  * */
-class DigitalInputCounter : public DigitalInput, public IntProducer {
+class DigitalInputCounter : public DigitalInput, public SensorT<int> {
  public:
   DigitalInputCounter(uint8_t pin, int pin_mode, int interrupt_type,
-                      unsigned int read_delay, String config_path = "");
+                      unsigned int read_delay, String config_path = "")
+      : DigitalInput{pin, pin_mode},
+        SensorT<int>(config_path),
+        read_delay_{read_delay} {
+    load_configuration();
+  }
 
-  void start() override final;
+  void start() override final {
+    ReactESP::app->onInterrupt(pin_, interrupt_type_, [this]() { this->counter_++; });
+
+    ReactESP::app->onRepeat(read_delay_, [this]() {
+      noInterrupts();
+      output = counter_;
+      counter_ = 0;
+      interrupts();
+      notify();
+    });
+  }
 
  private:
+  int interrupt_type_;
   unsigned int read_delay_;
   volatile unsigned int counter_ = 0;
   virtual void get_configuration(JsonObject& doc) override;
@@ -103,16 +116,14 @@ class DigitalInputCounter : public DigitalInput, public IntProducer {
 };
 
 /**
- * @brief DigitalInputChange is meant to report when a digital pin changes its
+ * @brief DigitalInputChange provides output whenever a digital pin changes its
  * state.
  *
- * It's intended for monitoring a button or other type of relatively slow
- * changing switch. Because physical switches like buttons can be somewhat
- * "noisy", you may want to connect the output of this Sensor to the Debounce
- * Transform, and try different combinations of read_delay for this Sensor and
- * ms_min_delay for Debounce to get the "cleanest" output from your button or
- * other switch. (Make Debounce::ms_min_delay at least a little longer than
- * DigitalInputChange::read_delay)
+ * DigitalInputChange is triggered by an interrupt on the pin, so it is
+ * guaranteed to catch all state changes on the pin. However, output reporting
+ * occurs during the ReactESP event loop, so for extremely fast state changes
+ * (with a frequency of at least several tens of kHz), it is possible that the
+ * event loop can't keep up and only the latest value is processed.)
  *
  * @param pin The GPIO pin to which the device is connected
  *
@@ -120,26 +131,32 @@ class DigitalInputCounter : public DigitalInput, public IntProducer {
  *
  * @param interrupt_type Will be RISING, FALLING, or CHANGE
  *
- * @param read_delay How often you want to read the state of the pin
- *
- * @param config_path The path to configure read_delay_ in the Config UI
+ * @param config_path The path to configure Sensor parameters, if any
  *
  * @see Debounce
  * */
-class DigitalInputChange : public DigitalInput, public BoolProducer {
+class DigitalInputChange : public DigitalInput, public SensorT<bool> {
  public:
   DigitalInputChange(uint8_t pin, int pin_mode, int interrupt_type,
-                     unsigned int read_delay = 10, String config_path = "");
+                     String config_path = "")
+      : DigitalInput{pin, pin_mode},
+        SensorT<bool>(config_path),
+        interrupt_type_{interrupt_type},
+        triggered_{true} {
+    load_configuration();
+    output = (bool)digitalRead(pin_);
+    last_output_ = !output;  // ensure that we always send the first output
+  }
   virtual void start() override final;
 
  private:
-  unsigned int read_delay_;
+  int interrupt_type_;
   bool triggered_;
-  uint8_t last_output_;
-  bool value_sent_;
-  virtual void get_configuration(JsonObject& doc) override;
-  virtual bool set_configuration(const JsonObject& config) override;
-  virtual String get_config_schema() override;
+  bool last_output_;
+  virtual void get_configuration(JsonObject& doc) override {}
+  virtual bool set_configuration(const JsonObject& config) override {
+    return false;
+  }
 };
 
 }  // namespace sensesp
