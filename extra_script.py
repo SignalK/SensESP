22dcf0aaaa97f9c0ad6e9f6e7f18d9dd69f5ebcb
Jan Dytrych <info@johnyseven.cz>
2022-01-26 08:41:00 +0100

    Fixed terser minify

--- extra_script.py
+++ extra_script.py
@@ -40,7 +40,7 @@ def make_c_header(inName, outName):
 
 
 def build_webUI(*args, **kwargs):
-   env.Execute("terser --compress --output /web/docroot/js/sensesp.min.js -- /web/docroot/js/sensesp.js")
+   env.Execute("terser --compress --output web/docroot/js/sensesp.min.js -- web/docroot/js/sensesp.js")
    make_c_header("js/sensesp.min.js", "js_sensesp")
    make_c_header("js/jsoneditor.min.js", "js_jsoneditor")
    make_c_header("css/bootstrap.min.css", "css_bootstrap")
