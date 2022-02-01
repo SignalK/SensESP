719bd474f50cd856c1ddbccf935e3296802406a5
Matti Airas <mairas@iki.fi>
2022-02-01 10:35:14 +0200

    Add a missing namespace declaration and const keyword

--- src/sensesp/sensesp_version.h
+++ src/sensesp/sensesp_version.h
@@ -1,6 +1,10 @@
 #ifndef __SRC_SENSESP_SENSESP_VERSION_H_
-#define __SRC_SENSESP_SENSESP_VERSION_H_ 
+#define __SRC_SENSESP_SENSESP_VERSION_H_
 
-const char* kSensESPVersion = "2.0.1-alpha";
+namespace sensesp {
+
+const char* const kSensESPVersion = "2.0.1-alpha";
+
+}
 
 #endif
