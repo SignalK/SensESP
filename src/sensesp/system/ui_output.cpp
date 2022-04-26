91a48e1c4062a4c035c3af12edc0376595b9589d
Matti Airas <mairas@iki.fi>
2022-04-26 13:17:05 +0300

    WIP UIOutput groups and ordering

--- src/sensesp/system/ui_output.cpp
+++ src/sensesp/system/ui_output.cpp
@@ -4,9 +4,4 @@ namespace sensesp {
 
 std::map<String, UIOutputBase*> UIOutputBase::ui_outputs_;
 
-UIOutputBase::UIOutputBase(String name) {
-  name_ = name;
-
-  ui_outputs_[name] = this;
-}
 }  // namespace sensesp
--- src/sensesp/system/ui_output.h
+++ src/sensesp/system/ui_output.h
@@ -12,13 +12,22 @@
 
 namespace sensesp {
 
+constexpr char kUIOutputDefaultGroup[] = "Default";
+constexpr int kUIOutputDefaultOrder = 1000;
+
 class UIOutputBase : virtual public Observable {
  protected:
   String name_;
+  String group_ = kUIOutputDefaultGroup;
+  int order_ = kUIOutputDefaultOrder;
   static std::map<String, UIOutputBase*> ui_outputs_;
 
  public:
-  UIOutputBase(String name);
+  UIOutputBase(String name, String group, int order)
+      : name_(name), group_(group), order_(order) {
+    ui_outputs_[name] = this;
+  }
+
   String& get_name() { return name_; }
 
   virtual void set_json(const JsonObject& obj) {}
@@ -34,14 +43,19 @@ class UILambdaOutput : public UIOutputBase {
   std::function<T()> value_function_;
 
  public:
-  UILambdaOutput(String name, std::function<T()> function)
-      : UIOutputBase(name) {
-    value_function_ = function;
-  }
+  UILambdaOutput(String name, std::function<T()> function,
+                 String group = kUIOutputDefaultGroup,
+                 int order = kUIOutputDefaultOrder)
+      : UIOutputBase(name, group, order), value_function_(function) {}
 
   T get() { return value_function_(); }
 
-  void set_json(const JsonObject& obj) override { obj[name_] = get(); }
+  void set_json(const JsonObject& obj) override {
+    JsonObject output = obj.createNestedObject(name_);
+    output["Value"] = get();
+    output["Group"] = group_;
+    output["Order"] = order_;
+  }
 };
 
 extern std::map<String, UIOutputBase*> ui_outputs;
@@ -51,14 +65,20 @@ class UIOutput : public UIOutputBase,
                  public ObservableValue<T>,
                  public ValueConsumer<T> {
  public:
-  UIOutput(String name) : UIOutputBase(name) {}
+  UIOutput(String name)
+      : UIOutputBase(name, kUIOutputDefaultGroup, kUIOutputDefaultOrder) {}
 
-  UIOutput(String name, T value) : UIOutputBase(name) {
+  UIOutput(String name, T value, String group = kUIOutputDefaultGroup,
+           int order = kUIOutputDefaultOrder)
+      : UIOutputBase(name, group, order) {
     this->ObservableValue<T>::emit(value);
   }
 
   void set_json(const JsonObject& obj) override {
-    obj[name_] = this->ObservableValue<T>::get();
+    JsonObject output = obj.createNestedObject(name_);
+    output["Value"] = ObservableValue<T>::get();
+    output["Group"] = group_;
+    output["Order"] = order_;
   }
 
   void set_input(T new_value, uint8_t input_channel = 0) override {
