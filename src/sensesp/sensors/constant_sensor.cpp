633d98dc9be8576ba4ceb4e334db8a5e9ff54e2a
Johan du Rand <johan.github@mestafin.com>
2023-06-21 20:16:40 +0200

    added constant_sensor class

---
+++ src/sensesp/sensors/constant_sensor.cpp
@@ -0,0 +1,63 @@
+#include "constant_sensor.h"
+
+namespace sensesp {
+
+// ..........................................
+// base class for constant sensors
+// ..........................................
+
+ConstantSensor::ConstantSensor(int send_delay, String config_path = "")
+    : SensorT<float>(config_path), send_delay_{send_delay} {
+  load_configuration();
+}
+
+void ConstantSensor::start() {
+  ReactESP::app->onRepeat(send_delay_ * 1000, [this]() { this->emit(value_); });
+}
+
+void ConstantSensor::update() { this->emit(value_); }
+
+String ConstantSensor::get_config_schema() {
+  return FPSTR(SCHEMA_CONSTANT_SENSOR);
+}
+
+void ConstantSensor::get_configuration(JsonObject &root) {
+  root["value"] = value_;
+}
+
+bool ConstantSensor::set_configuration(const JsonObject &config) {
+  String expected[] = {"value"};
+  for (auto str : expected) {
+    if (!config.containsKey(str)) {
+      return false;
+    }
+  }
+  value_ = config["value"];
+  return true;
+}
+
+// ..........................................
+// constant sensors
+// ..........................................
+
+//
+// constant float
+ConstantFloat::ConstantFloat(int send_delay, String config_path = "")
+    : ConstantSensor(send_delay, config_path) {}
+void ConstantFloat::setValue(float value) { value_ = value; }
+float ConstantFloat::getValue() { return value_; }
+
+// constant int
+ConstantInt::ConstantInt(int send_delay, String config_path = "")
+    : ConstantSensor(send_delay, config_path) {}
+void ConstantInt::setValue(int value) { value_ = value; }
+int ConstantInt::getValue() { return value_; }
+
+//
+// constant String
+ConstantString::ConstantString(int send_delay, String config_path = "")
+    : ConstantSensor(send_delay, config_path) {}
+void ConstantString::setValue(String value) { value_ = value; }
+String ConstantString::getValue() { return value_; }
+
+}  // namespace sensesp
---
+++ src/sensesp/sensors/constant_sensor.h
@@ -0,0 +1,94 @@
+#ifndef CONSTANT_SENSOR_H_
+#define CONSTANT_SENSOR_H_
+
+#include "Arduino.h"
+#include "sensesp/sensors/sensor.h"
+
+namespace sensesp {
+
+/**
+ * @brief ConstantSensor is the base class for virtual sensors that periodically
+ * emit a constant value.
+ *
+ * The virtual sensors are typically used to send a constant value to SignalK.
+ * Typical example is the capacity of a tank.
+ *
+ * The sensors can be connected to all the standard SKOutput consumers.
+ *
+ * The sensor value and send delay can be configured via the web ui.
+ *
+ * The sensor has a getValue() and a setValue() method to interact with the
+ * sensor from normal code;
+ *
+ * @param send_delay[in] Time delay in seconds between consecutive emissions of
+ * the sensor value.
+ *
+ * @param[in] config_path Configuration path for the sensor.
+ */
+
+static const char SCHEMA_CONSTANT_SENSOR[] PROGMEM = R"###({
+        "type": "object",
+        "properties": {
+            "value": { "title": "Constant Value", "type": "number", "description": "Constant value" }
+        }
+    })###";
+
+// ..........................................
+//  base class for constant value sensors
+// ..........................................
+class ConstantSensor : public SensorT<float> {
+ public:
+  ConstantSensor(int send_delay, String config_path);
+  void start() override final;
+
+ protected:
+  virtual void get_configuration(JsonObject &doc) override;
+  virtual bool set_configuration(const JsonObject &config) override;
+  virtual String get_config_schema() override;
+  void update();
+
+ private:
+  float value_;
+  int send_delay_ = 30;  // seconds
+};
+
+// ..........................................
+//  constant value sensors
+// ..........................................
+
+// float constant
+class ConstantFloat : public ConstantSensor {
+ public:
+  ConstantFloat(int send_delay, String config_path);
+  void setValue(float value);
+  float getValue();
+
+ private:
+  float value_ = 0;
+};
+
+// float constant
+class ConstantInt : public ConstantSensor {
+ public:
+  ConstantInt(int send_delay, String config_path);
+  void setValue(int value);
+  int getValue();
+
+ private:
+  int value_ = 0;
+};
+
+// String constant
+class ConstantString : public ConstantSensor {
+ public:
+  ConstantString(int send_delay, String config_path);
+  void setValue(String value);
+  String getValue();
+
+ private:
+  String value_ = "";
+};
+
+}  // namespace sensesp
+
+#endif
