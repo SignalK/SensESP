6cc7dd70f5a49d1e7c2eef1345e8b48fb5cd60f0
Matti Airas <matti.airas@hatlabs.fi>
2023-11-26 22:29:59 +0200

    Implement /api/routes endpoint

--- src/sensesp/net/web/command_handler.cpp
+++ src/sensesp/net/web/command_handler.cpp
@@ -2,11 +2,20 @@
 
 namespace sensesp {
 
+void HTTPRoutesHandler::get_routes() {
+    routes_.push_back(RouteDefinition("Status", "/status", "StatusPage"));
+    routes_.push_back(RouteDefinition("System", "/system", "SystemPage"));
+    routes_.push_back(RouteDefinition("WiFi", "/wifi", "WiFiConfigPage"));
+    routes_.push_back(RouteDefinition("Signal K", "/signalk", "SignalKPage"));
+    routes_.push_back(RouteDefinition("Configuration", "/configuration",
+                                     "ConfigurationPage"));
+  }
+
 void add_http_command_handlers(HTTPServer* server) {
-  (new HTTPResetHandler())->set_handler(server);
-  (new HTTPRestartHandler())->set_handler(server);
-  (new HTTPInfoHandler())->set_handler(server);
-  (new HTTPRoutesHandler())->set_handler(server);
+  HTTPResetHandler* reset_handler = new HTTPResetHandler();
+  HTTPRestartHandler* restart_handler = new HTTPRestartHandler();
+  HTTPInfoHandler* info_handler = new HTTPInfoHandler();
+  HTTPRoutesHandler* routes_handler = new HTTPRoutesHandler();
 }
 
 }  // namespace sensesp
--- src/sensesp/net/web/command_handler.h
+++ src/sensesp/net/web/command_handler.h
@@ -59,12 +59,15 @@ class HTTPInfoHandler : public HTTPServerHandler {
   HTTPInfoHandler() : HTTPServerHandler(){};
   virtual void set_handler(HTTPServer* server) override {
     // handler for GET /api/info
-    const httpd_uri_t info_handler = {.uri = "/api/info",
-                                      .method = HTTP_GET,
-                                      .handler = [](httpd_req_t* req) {
-                                        return call_member_handler(
-                                            req, &HTTPInfoHandler::handle_get);
-                                      }};
+    const httpd_uri_t info_handler = {
+        .uri = "/api/info",
+        .method = HTTP_GET,
+        .handler =
+            [](httpd_req_t* req) {
+              return call_member_handler(req, &HTTPInfoHandler::handle_get);
+            },
+        .user_ctx = (void*)this,
+    };
     server->register_handler(&info_handler);
   };
 
@@ -73,23 +76,15 @@ class HTTPInfoHandler : public HTTPServerHandler {
     auto ui_outputs = UIOutputBase::get_ui_outputs();
 
     std::size_t output_buffer_size =
-        (200 * configurables.size() +  // configuration cards
-         200 * ui_outputs->size())     // status output entities
-
+        (200 * ui_outputs->size())     // status output entities
         + 512;
     DynamicJsonDocument json_doc(output_buffer_size);
+    JsonArray info_items = json_doc.to<JsonArray>();
 
-    auto properties = json_doc.createNestedObject("Properties");
-    auto commands = json_doc.createNestedArray("Commands");
-    auto pages = json_doc.createNestedArray("Pages");
-    auto config = json_doc.createNestedArray("Config");
-
-    for (auto property = ui_outputs->begin(); property != ui_outputs->end();
-         ++property) {
-      property->second->set_json(properties);
+    for (auto info_item = ui_outputs->begin(); info_item != ui_outputs->end();
+         ++info_item) {
+      info_items.add(info_item->second->as_json());
     }
-
-    add_sorted_configurables(config);
 
     String response;
     serializeJson(json_doc, response);
@@ -97,6 +92,11 @@ class HTTPInfoHandler : public HTTPServerHandler {
     httpd_resp_sendstr(req, response.c_str());
     return ESP_OK;
   }
+};
+
+class HTTPConfigListHandler : public HTTPServerHandler {
+ public:
+  HTTPConfigListHandler() : HTTPServerHandler(){};
 
   void add_sorted_configurables(JsonArray& config) {
     // sort the configurables by their sort_order
@@ -124,13 +124,12 @@ class RouteDefinition {
   String get_component_name() { return component_name_; }
 
   DynamicJsonDocument as_json() {
-    int buffer_size = JSON_OBJECT_SIZE(3) + 100;
-    DynamicJsonDocument json_doc(buffer_size);
-    json_doc["name"] = name_;
-    json_doc["path"] = path_;
-    json_doc["componentName"] = component_name_;
+    DynamicJsonDocument doc(200);
+    doc["name"] = name_;
+    doc["path"] = path_;
+    doc["componentName"] = component_name_;
 
-    return json_doc;
+    return doc;
   }
 
  protected:
@@ -141,50 +140,46 @@ class RouteDefinition {
 
 class HTTPRoutesHandler : public HTTPServerHandler {
  public:
-  HTTPRoutesHandler() : HTTPServerHandler(){};
+  HTTPRoutesHandler() : HTTPServerHandler() {
+    this->get_routes();
+  };
+
   virtual void set_handler(HTTPServer* server) override {
     // handler for GET /device/restart
     const httpd_uri_t routes_handler = {
         .uri = "/api/routes",
         .method = HTTP_GET,
-        .handler = [](httpd_req_t* req) {
-          return call_member_handler(req, &HTTPRoutesHandler::handle_get);
-        }};
+        .handler =
+            [](httpd_req_t* req) {
+              return call_member_handler(req, &HTTPRoutesHandler::handle_get);
+            },
+        .user_ctx = (void*)this,
+    };
     server->register_handler(&routes_handler);
   };
 
  protected:
-  esp_err_t handle_get(httpd_req_t* req) {
-    std::vector<RouteDefinition> routes = get_routes();
+  std::vector<RouteDefinition> routes_;
 
-    int buffer_size = JSON_ARRAY_SIZE(routes.size()) +
-                      routes.size() * JSON_OBJECT_SIZE(3) + 100;
+  esp_err_t handle_get(httpd_req_t* req) {
+    int buffer_size = 4000;
     DynamicJsonDocument json_doc(buffer_size);
     JsonArray routes_json = json_doc.to<JsonArray>();
 
-    for (auto it = routes.begin(); it != routes.end(); ++it) {
+    int sz = routes_.size();
+
+    for (auto it = routes_.begin(); it != routes_.end(); ++it) {
       routes_json.add(it->as_json());
     }
 
     String response;
-    serializeJson(json_doc, response);
+    serializeJson(routes_json, response);
     httpd_resp_set_type(req, "application/json");
     httpd_resp_sendstr(req, response.c_str());
     return ESP_OK;
   }
 
-  virtual std::vector<RouteDefinition> get_routes() {
-
-    std::vector<RouteDefinition> routes;
-    routes.push_back(RouteDefinition("Status", "/status", "StatusPage"));
-    routes.push_back(RouteDefinition("System", "/system", "SystemPage"));
-    routes.push_back(RouteDefinition("WiFi", "/wifi", "WiFiConfigPage"));
-    routes.push_back(RouteDefinition("Signal K", "/signalk", "SignalKPage"));
-    routes.push_back(RouteDefinition("Configuration", "/configuration",
-                                     "ConfigurationPage"));
-
-    return routes;
-  }
+  virtual void get_routes();
 };
 
 void add_http_command_handlers(HTTPServer* server);
