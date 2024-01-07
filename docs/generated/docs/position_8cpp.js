bf9288ab57a3ccabf98993de264cbb281f122302
Matti Airas <matti.airas@hatlabs.fi>
2024-01-07 16:35:47 +0200

    Update the Doxygen docs for version 2.7.2

---
+++ docs/generated/docs/position_8cpp.js
@@ -0,0 +1,5 @@
+var position_8cpp =
+[
+    [ "canConvertFromJson", "position_8cpp.html#aa3eacebd09da35468c9660c3d97a4cf4", null ],
+    [ "convertFromJson", "position_8cpp.html#aab7b7844907c703d059afecbd91f6bdc", null ]
+];
