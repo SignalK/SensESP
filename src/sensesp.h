f821a6ef368777f8b59db621ab7c80c7b3805262
Matti Airas <matti.airas@hatlabs.fi>
2024-05-25 21:21:49 +0300

    Throw an error if CORE_DEBUG_LEVEL is not defined

--- src/sensesp.h
+++ src/sensesp.h
@@ -10,6 +10,10 @@
     "ESP8266 isn't supported by SensESP version 2. To compile an existing project, add @^1.0.0 to the SensESP depdenency in platformio.ini."
 #endif
 
+#ifndef CORE_DEBUG_LEVEL
+#error "SensESP v3 requires CORE_DEBUG_LEVEL to be defined. See https://signalk.org/SensESP/pages/migration/."
+#endif
+
 namespace sensesp {
 
 // Typically, pulling a namespace into another is something to be avoided,
