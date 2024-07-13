e97a6feffaf1a1c9c8b94706da5f618cab839e3d
Matti Airas <matti.airas@hatlabs.fi>
2024-07-13 20:19:21 +0200

    Implement a filter transform that emits its input conditionally

---
+++ src/sensesp/transforms/filter.h
@@ -0,0 +1,34 @@
+#ifndef SENSESP_SRC_TRANSFORMS_FILTER_H_
+#define SENSESP_SRC_TRANSFORMS_FILTER_H_
+
+#include <functional>
+
+namespace sensesp {
+
+/**
+ * @brief Transform that only emits the output if the filter condition returns
+ * true.
+ *
+ */
+template <typename T>
+class Filter : public Transform<T, T> {
+  public:
+    Filter(std::function<bool(const T&)> filter, String config_path = "")
+        : Transform<T, T>(config_path), filter_{filter} {
+      this->load_configuration();
+    }
+    virtual void set(const T& new_value) override {
+      if (filter_(new_value)) {
+        this->emit(new_value);
+      }
+    }
+
+  private:
+    std::function<bool(const T&)> filter_;
+
+};
+
+
+}  // namespace sensesp
+
+#endif  // SENSESP_SRC_TRANSFORMS_FILTER_H_
