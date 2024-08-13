38653d85dc3308d2ccdb9dc59bf1343f0157eb63
Matti Airas <matti.airas@hatlabs.fi>
2024-08-13 17:12:06 +0300

    Provide a dummy Startable class for compatibility

---
+++ src/sensesp/system/startable.h
@@ -0,0 +1,21 @@
+#ifndef SENSESP_SRC_SENSESP_SYSTEM_STARTABLE_H_
+#define SENSESP_SRC_SENSESP_SYSTEM_STARTABLE_H_
+
+namespace sensesp {
+
+/**
+ * @brief Dummy Startable class for backward compatibility.
+ *
+ */
+class Startable {
+ public:
+  Startable(int priority = 0) {}
+  virtual void start() {}
+  const int get_start_priority() { return 0; }
+  void set_start_priority(int priority) {}
+  static void start_all() {}
+};
+
+}  // namespace sensesp
+
+#endif  // SENSESP_SRC_SENSESP_SYSTEM_STARTABLE_H_
