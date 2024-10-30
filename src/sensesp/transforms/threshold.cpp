541170e824dea9599797a7c253b65bb322659443
Matti Airas <matti.airas@hatlabs.fi>
2024-10-30 11:40:38 +0200

    Fix the config schema
    It was missing a comma.

--- src/sensesp/transforms/threshold.cpp
+++ src/sensesp/transforms/threshold.cpp
@@ -2,15 +2,8 @@
 
 namespace sensesp {
 
-static const char kSchemaTemplate[] PROGMEM = R"({
-    "type": "object",
-    "properties": {
-        "min": { "title": "Minimum value", "type": "!!TYPE!!", "description" : "Minimum value to be 'in range'" },
-        "max": { "title": "Maximum value", "type": "!!TYPE!!", "description" : "Maximum value to be 'in range'" },
-        "in_range": { "title": "In range value", "type": "boolean", "description" : "Output value when input value is 'in range'" }
-        "out_range": { "title": "Out of range value", "type": "boolean", "description" : "Output value when input value is out of range" }
-    }
-  })";
+static const char kSchemaTemplate[] =
+    R"({"type":"object","properties":{"min":{"title":"Minimum value","type":"!!TYPE!!","description":"Minimum value to be 'in range'"},"max":{"title":"Maximum value","type":"!!TYPE!!","description":"Maximum value to be 'in range'"},"in_range":{"title":"In range value","type":"boolean","description":"When checked, output true when input value is 'in range'"}}})";
 
 const String ConfigSchema(const ThresholdTransform<float>& obj) {
   String schema = kSchemaTemplate;
