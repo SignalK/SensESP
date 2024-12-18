640524f8d7864cdd096acaf30aabdbcb52f29839
Matti Airas <matti.airas@hatlabs.fi>
2024-12-18 10:31:15 +0200

    Use env variables to customize the ini file

--- ci/run-ci.sh
+++ ci/run-ci.sh
@@ -4,11 +4,8 @@ set -euo pipefail
 
 # assume this command is always run from the project root directory
 
-PROJDIR=$(pwd)
-
-# replace the 
-sed -e "s|\${PROJDIR}|$PROJDIR|" ci/platformio-${CI_TARGET_DEVICE}.ini > ci/platformio.ini
+export PROJDIR=$(pwd)
 
 # the example to build comes from $PLATFORMIO_CI_SRC
 
-pio ci -c ci/platformio.ini
+pio ci -c ci/platformio_${CI_PLATFORM}.ini
