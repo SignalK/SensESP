02674c4973f6a3c30505cd1be8f7596f3a2da0b4
zweistein22 <30336416+zweistein22@users.noreply.github.com>
2026-03-11 21:20:26 +0100

    Enhance SKPutListener constructor with debug logs
    Added debug logging for listener count in SKPutListener constructor.

--- src/sensesp/signalk/signalk_put_request_listener.cpp
+++ src/sensesp/signalk/signalk_put_request_listener.cpp
@@ -5,8 +5,10 @@ namespace sensesp {
 std::vector<SKPutListener*> SKPutListener::listeners_;
 
 SKPutListener::SKPutListener(const String& sk_path) : sk_path{sk_path} {
-  debugI("SKPutListener(%s)",sk_path.c_str());
+  
   listeners_.push_back(this);
+  debugI("SKPutListener(%s)",sk_path.c_str());
+   debugI("===========   listeners_.size()=%d    ===========",listeners_.size());
 }
 
 }  // namespace sensesp
