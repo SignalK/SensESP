9eafb48a20ab2e819cb2970ac52c367542bc7737
zweistein22 <30336416+zweistein22@users.noreply.github.com>
2026-03-11 22:46:29 +0100

    Fix static member access in SKPutListener

--- src/sensesp/signalk/signalk_put_request_listener.cpp
+++ src/sensesp/signalk/signalk_put_request_listener.cpp
@@ -2,13 +2,13 @@
 
 namespace sensesp {
 
-//std::vector<SKPutListener*> SKPutListener::listeners_;
+std::vector<SKPutListener*> SKPutListener::listeners_;
 
 SKPutListener::SKPutListener(const String& sk_path) : sk_path{sk_path} {
   
-  listeners_.push_back(this);
+  SKPutListener::listeners_.push_back(this);
   debugI("SKPutListener(%s)",sk_path.c_str());
-   debugI("===========   SKPutListener::listeners_.size()=%d    ===========",listeners_.size());
+   debugI("===========   SKPutListener::listeners_.size()=%d    ===========",SKPutListener::listeners_.size());
 }
 
 }  // namespace sensesp
