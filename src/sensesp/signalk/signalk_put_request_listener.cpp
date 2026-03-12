625ab6580f28b86edceed43eee26f1273c8dde7e
zweistein22 <30336416+zweistein22@users.noreply.github.com>
2026-03-12 04:37:27 +0100

    Add JSON serialization methods to SKPutListener

--- src/sensesp/signalk/signalk_put_request_listener.cpp
+++ src/sensesp/signalk/signalk_put_request_listener.cpp
@@ -11,4 +11,22 @@ SKPutListener::SKPutListener(const String& sk_path) : sk_path{sk_path} {
    debugI("===========   SKPutListener::listeners_.size()=%d    ===========",SKPutListener::listeners_.size());
 }
 
+bool SKPutListener::to_json(JsonObject &root) {
+  root["sk_path"] = this->get_sk_path();
+  return true;
+}
+
+bool SKPutListener::from_json(const JsonObject &config) {
+  if (!config["sk_path"].is<String>()) {
+    return false;
+  }
+  this->set_sk_path(config["sk_path"].as<String>());
+  return true;
+}
+
+void SKPutListener::set_sk_path(const String &path) { sk_path = path; }
+
+const String ConfigSchema(const SKPutListener &obj) {
+  return R"({"type":"object","properties":{"listen_delay":{"title":"Listen delay","type":"number","description":"The time, in milliseconds, between each read of the input"}}  })";
+}
 }  // namespace sensesp
